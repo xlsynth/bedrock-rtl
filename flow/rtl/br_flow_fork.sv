5422944de825494bd6c81d3e0dcf67c959abccd7
Mark Gottscho <mgottscho@openai.com>
2024-09-20 17:50:53 -0700

    fix br_flow_fork

--- flow/rtl/br_flow_fork.sv
+++ flow/rtl/br_flow_fork.sv
@@ -24,7 +24,7 @@
 `include "br_asserts_internal.svh"
 
 module br_flow_fork #(
-    parameter int NumForks = 2  // Must be at least 2
+    parameter int NumFlows = 2  // Must be at least 2
 )(
     input logic clk,  // Used only for assertions
     input logic rst,  // Used only for assertions
@@ -33,38 +33,40 @@ module br_flow_fork #(
     output logic push_ready,
     input  logic push_valid,
 
-    // Forked pop-side interfaces
+    // Pop-side interfaces
     //
     // pop_valid signals are unstable because they must fall if another pop_ready falls.
     // There is no dependency between pop_valid[i] and pop_ready[i].
-    input  logic [NumForks-1:0] pop_ready,
-    output logic [NumForks-1:0] pop_valid_unstable
+    input  logic [NumFlows-1:0] pop_ready,
+    output logic [NumFlows-1:0] pop_valid_unstable
 );
 
     //------------------------------------------
     // Integration checks
     //------------------------------------------
-    `BR_ASSERT_STATIC(NumForksMustBeAtLeastTwo_A, NumForks >= 2)
+    `BR_ASSERT_STATIC(NumFlowsMustBeAtLeastTwo_A, NumFlows >= 2)
     `BR_ASSERT_INTG(push_backpressure_intg_A, !push_ready && push_valid |=> push_valid)
 
     //------------------------------------------
     // Implementation
     //------------------------------------------
-    for (int i = 0; i < NumForks; i++) begin : gen_forks
+    assign push_ready = &pop_ready;
+
+    for (int i = 0; i < NumFlows; i++) begin : gen_flows
         always_comb begin
-            pop_valid[i] = push_valid;
-            for (int j = 0; j < NumForks; j++) begin
+            pop_valid_unstable[i] = push_valid;
+            for (int j = 0; j < NumFlows; j++) begin
                 if (i != j) begin
-                    pop_valid[i] &= pop_ready[j];
+                    pop_valid_unstable[i] &= pop_ready[j];
                 end
             end
         end
     end
-    
+ 
     //------------------------------------------
     // Implementation checks
     //------------------------------------------
-    for (int i = 0; i < NumForks; i++) begin : gen_fork_checks
+    for (int i = 0; i < NumFlows; i++) begin : gen_flow_checks
         `BR_COVER_IMPL(pop_valid_unstable_C, $stable(push_valid) && $fell(pop_valid[i]))
     end
 
