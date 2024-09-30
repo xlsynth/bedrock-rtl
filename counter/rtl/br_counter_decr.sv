41d4e2b718112e8b90662a58bdc049472f0fb240
Mark Gottscho <mgottscho@openai.com>
2024-09-30 11:24:18 -0700

    Implement br_counter_decr and fix some things in incr version (#19)

--- counter/rtl/br_counter_decr.sv
+++ counter/rtl/br_counter_decr.sv
@@ -12,40 +12,40 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Bedrock-RTL Incrementing Counter
+// Bedrock-RTL Decrementing Counter
 //
-// A simple counter that increments by a potentially variable amount each cycle,
-// where the maximum increment is given by MaxIncrement (inclusive).
-// Overflows (wraps around past 0) at MaxValue (inclusive), even if MaxValue + 1
+// A simple counter that decrements by a potentially variable amount each cycle,
+// where the maximum decrement is given by MaxDecrement (inclusive).
+// Underflows (wraps around past 0) up to MaxValue (inclusive), even if MaxValue + 1
 // isn't a power-of-2. In the common case where MaxValue + 1 is a power-of-2,
 // the implementation is simplified.
 //
-// When there is a valid increment and it overflows:
-//   value_next = (value + incr) % (MaxValue + 1)
+// When there is a valid decrement and it underflows:
+//   value_next = (value - decr) % (MaxValue + 1)
 //
 // The counter state is exposed in two ways.
 // (1) value holds the current counter state. There is a latency of 1 cycle from
-//     a valid increment to the counter state being updated.
+//     a valid decrement to the counter state being updated.
 // (2) value_next is what value will be on the next cycle. It is conditioned on
-//     incr_valid: if low, then value_next == value. This is useful for constructing
+//     decr_valid: if low, then value_next == value. This is useful for constructing
 //     single-cycle chains of counters.
 // value and value_next are always valid.
 //
-// The value resets to 0.
+// The value resets to MaxValue.
 
 `include "br_asserts_internal.svh"
 `include "br_registers.svh"
 
-module br_counter_incr #(
-    parameter int MaxValue = 1,  // Must be at least 1. Inclusive.
-    parameter int MaxIncrement = 1,  // Must be at least 1. Inclusive.
+module br_counter_decr #(
+    parameter int MaxValue = 1,  // Must be at least 1. Inclusive. Also the initial value.
+    parameter int MaxDecrement = 1,  // Must be at least 1. Inclusive.
     localparam int ValueWidth = $clog2(MaxValue + 1),
-    localparam int IncrementWidth = $clog2(MaxIncrement + 1)
+    localparam int DecrementWidth = $clog2(MaxDecrement + 1)
 ) (
     input  logic                      clk,
     input  logic                      rst,
-    input  logic                      incr_valid,
-    input  logic [IncrementWidth-1:0] incr,
+    input  logic                      decr_valid,
+    input  logic [DecrementWidth-1:0] decr,
     output logic [    ValueWidth-1:0] value,
     output logic [    ValueWidth-1:0] value_next
 );
@@ -54,54 +54,57 @@ module br_counter_incr #(
   // Integration checks
   //------------------------------------------
   `BR_ASSERT_STATIC(MaxValue_gte_1_A, MaxValue >= 1)
-  `BR_ASSERT_STATIC(MaxIncrement_gte_1_A, MaxIncrement >= 1)
+  `BR_ASSERT_STATIC(MaxDecrement_gte_1_A, MaxDecrement >= 1)
 
-  `BR_ASSERT_INTG(incr_in_range_A, incr_valid |-> incr <= MaxIncrement)
+  `BR_ASSERT_INTG(decr_in_range_A, decr_valid |-> decr <= MaxDecrement)
 
   //------------------------------------------
   // Implementation
   //------------------------------------------
   localparam int MaxValuePlusOne = MaxValue + 1;
   localparam bit IsMaxValuePlusOnePowerOf2 = (MaxValuePlusOne & (MaxValuePlusOne - 1)) == 0;
-  localparam int TempWidth = ValueWidth + IncrementWidth;
 
   logic [ValueWidth-1:0] value_next_internal;
-  logic [ TempWidth-1:0] value_temp;
+  logic [ValueWidth-1:0] value_temp;
 
-  assign value_temp = value + incr;
+  assign value_temp = value - decr;
 
   if (IsMaxValuePlusOnePowerOf2) begin : gen_power_of_2
     // For MaxValuePlusOne being a power of 2, wrapping occurs naturally
-    assign value_next_internal = incr_valid ? value_temp : value;
+    assign value_next_internal = decr_valid ? value_temp : value;
 
   end else begin : gen_non_power_of_2
     // For MaxValuePlusOne not being a power of 2, handle wrap-around explicitly
-    logic [TempWidth-1:0] value_temp_wrapped;
-    assign value_temp_wrapped = value_temp - MaxValue - 1;
-    assign value_next_internal = (value_temp >= MaxValue) ?
-      value_temp_wrapped[ValueWidth-1:0] :
-      value_temp[ValueWidth-1:0];
+    localparam int Margin = 2 ** ValueWidth - 1 - MaxValue;
+    logic [ValueWidth-1:0] value_temp_wrapped;
+    assign value_temp_wrapped  = value_temp - Margin;
+    assign value_next_internal = value_temp >= MaxValue ? value_temp_wrapped : value_temp;
+
+    // Case-specific implementation checks
+    `BR_ASSERT_STATIC(Margin_gte0_A, Margin > 0)
+    `BR_ASSERT_IMPL(value_temp_wrapped_in_range_A, value_temp_wrapped <= MaxValue)
   end
 
   // If the user leaves the value_next port unused, then the synthesis tool
   // should be able to automatically simplify the following ternary expression
   // to just value_next_internal, because the load-enable on the register is
-  // conditioned on the same incr_valid.
-  assign value_next = incr_valid ? value_next_internal : value;
-  `BR_REGL(value, value_next, incr_valid)
+  // conditioned on the same decr_valid.
+  assign value_next = decr_valid ? value_next_internal : value;
+  `BR_REGIL(value, value_next, decr_valid, MaxValue)
 
   //------------------------------------------
   // Implementation checks
   //------------------------------------------
+  `BR_ASSERT_IMPL(reset_to_MaxValue_A, $fell(rst) |-> value == MaxValue)
   `BR_ASSERT_IMPL(value_in_range_A, value <= MaxValue)
   `BR_ASSERT_IMPL(value_next_in_range_A, value_next <= MaxValue)
   `BR_ASSERT_IMPL(value_next_propagates_A, ##1 value == $past(value_next))
   `BR_ASSERT_IMPL(value_wrap_A,
-                  incr_valid && value_temp > MaxValue |-> value_next == value_temp - MaxValue - 1)
+                  decr_valid && value_temp > MaxValue |-> value_next == value_temp - MaxValue - 1)
   `BR_ASSERT_IMPL(maxvalue_plus_one_A,
-                  value == MaxValue && incr_valid && incr == 1'b1 |-> value_next == 0)
-  `BR_ASSERT_IMPL(plus_zero_A, incr_valid && incr == '0 |-> value_next == value)
-  `BR_COVER_IMPL(increment_max_C, incr_valid && incr == MaxIncrement)
+                  value == MaxValue && decr_valid && decr == 1'b1 |-> value_next == 0)
+  `BR_ASSERT_IMPL(plus_zero_A, decr_valid && decr == '0 |-> value_next == value)
+  `BR_COVER_IMPL(decrement_max_C, decr_valid && decr == MaxDecrement)
   `BR_COVER_IMPL(value_temp_oob_C, value_temp > MaxValue)
 
-endmodule : br_counter_incr
+endmodule : br_counter_decr
