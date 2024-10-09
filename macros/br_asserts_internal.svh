ad42f15bd2ba5abc30b5e8957c0c204f1ef59662
Mark Gottscho <mgottscho@openai.com>
2024-10-09 16:54:31 +0000

    fix

--- macros/br_asserts_internal.svh
+++ macros/br_asserts_internal.svh
@@ -40,13 +40,22 @@
 // verilog_lint: waive-start line-length
 // verilog_format: off
 
+`define BR_INTG_CHECK_NO_OP \
+// ri lint_check waive NULL_STMT \
+; // (macro no-op; BR_DISABLE_INTG_CHECKS defined)
+
+`define BR_IMPL_CHECK_NO_OP \
+// ri lint_check waive NULL_STMT \
+; // (macro no-op; BR_ENABLE_IMPL_CHECKS not defined)
+
 // Clock: 'clk'
 // Reset: 'rst'
 `ifndef BR_DISABLE_INTG_CHECKS
 `define BR_ASSERT_INTG(__name__, __expr__) \
 `BR_ASSERT(__name__, __expr__)
 `else  // BR_DISABLE_INTG_CHECKS
-`define BR_ASSERT_INTG(__name__, __expr__) ;  // macro no-op; BR_DISABLE_INTG_CHECKS defined
+`define BR_ASSERT_INTG(__name__, __expr__) \
+`BR_INTG_CHECK_NO_OP
 `endif  // BR_DISABLE_INTG_CHECKS
 
 // More expressive form of BR_ASSERT_INTG that allows the use of custom clock and reset signal names.
@@ -54,7 +63,8 @@
 `define BR_ASSERT_CR_INTG(__name__, __expr__, __clk__, __rst__) \
 `BR_ASSERT_CR(__name__, __expr__, __clk__, __rst__)
 `else  // BR_DISABLE_INTG_CHECKS
-`define BR_ASSERT_CR_INTG(__name__, __expr__, __clk__, __rst__) ;  // macro no-op; BR_DISABLE_INTG_CHECKS defined
+`define BR_ASSERT_CR_INTG(__name__, __expr__, __clk__, __rst__) \
+`BR_INTG_CHECK_NO_OP
 `endif  // BR_DISABLE_INTG_CHECKS
 
 // Clock: 'clk'
@@ -63,7 +73,8 @@
 `define BR_ASSERT_IMPL(__name__, __expr__) \
 `BR_ASSERT(__name__, __expr__)
 `else  // BR_ENABLE_IMPL_CHECKS
-`define BR_ASSERT_IMPL(__name__, __expr__) ;  // macro no-op; BR_ENABLE_IMPL_CHECKS not defined
+`define BR_ASSERT_IMPL(__name__, __expr__) \
+`BR_IMPL_CHECK_NO_OP
 `endif  // BR_ENABLE_IMPL_CHECKS
 
 // More expressive form of BR_ASSERT_IMPL that allows the use of custom clock and reset signal names.
@@ -71,7 +82,8 @@
 `define BR_ASSERT_CR_IMPL(__name__, __expr__, __clk__, __rst__) \
 `BR_ASSERT_CR(__name__, __expr__, __clk__, __rst__)
 `else  // BR_ENABLE_IMPL_CHECKS
-`define BR_ASSERT_CR_IMPL(__name__, __expr__, __clk__, __rst__) ;  // macro no-op; BR_ENABLE_IMPL_CHECKS not defined
+`define BR_ASSERT_CR_IMPL(__name__, __expr__, __clk__, __rst__) \
+`BR_IMPL_CHECK_NO_OP
 `endif  // BR_ENABLE_IMPL_CHECKS
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -81,14 +93,16 @@
 `define BR_ASSERT_COMB_INTG(__name__, __expr__) \
 `BR_ASSERT_COMB(__name__, __expr__)
 `else  // BR_DISABLE_INTG_CHECKS
-`define BR_ASSERT_COMB_INTG(__name__, __expr__) ;  // macro no-op; BR_DISABLE_INTG_CHECKS defined
+`define BR_ASSERT_COMB_INTG(__name__, __expr__) \
+`BR_INTG_CHECK_NO_OP
 `endif  // BR_DISABLE_INTG_CHECKS
 
 `ifdef BR_ENABLE_IMPL_CHECKS
 `define BR_ASSERT_COMB_IMPL(__name__, __expr__) \
 `BR_ASSERT_COMB(__name__, __expr__)
 `else  // BR_ENABLE_IMPL_CHECKS
-`define BR_ASSERT_COMB_IMPL(__name__, __expr__) ;  // macro no-op; BR_ENABLE_IMPL_CHECKS not defined
+`define BR_ASSERT_COMB_IMPL(__name__, __expr__) \
+`BR_IMPL_CHECK_NO_OP
 `endif  // BR_ENABLE_IMPL_CHECKS
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -101,7 +115,8 @@
 `define BR_COVER_INTG(__name__, __expr__) \
 `BR_COVER(__name__, __expr__)
 `else  // BR_DISABLE_INTG_CHECKS
-`define BR_COVER_INTG(__name__, __expr__) ;  // macro no-op; BR_DISABLE_INTG_CHECKS defined
+`define BR_COVER_INTG(__name__, __expr__) \
+`BR_INTG_CHECK_NO_OP
 `endif  // BR_DISABLE_INTG_CHECKS
 
 // More expressive form of BR_COVER_INTG that allows the use of custom clock and reset signal names.
@@ -109,7 +124,8 @@
 `define BR_COVER_CR_INTG(__name__, __expr__, __clk__, __rst__) \
 `BR_COVER_CR(__name__, __expr__, __clk__, __rst__)
 `else  // BR_DISABLE_INTG_CHECKS
-`define BR_COVER_CR_INTG(__name__, __expr__, __clk__, __rst__) ;  // macro no-op; BR_DISABLE_INTG_CHECKS defined
+`define BR_COVER_CR_INTG(__name__, __expr__, __clk__, __rst__) \
+`BR_INTG_CHECK_NO_OP
 `endif  // BR_DISABLE_INTG_CHECKS
 
 // Clock: 'clk'
@@ -118,7 +134,8 @@
 `define BR_COVER_IMPL(__name__, __expr__) \
 `BR_COVER(__name__, __expr__)
 `else  // BR_ENABLE_IMPL_CHECKS
-`define BR_COVER_IMPL(__name__, __expr__) ;  // macro no-op; BR_ENABLE_IMPL_CHECKS not defined
+`define BR_COVER_IMPL(__name__, __expr__) \
+`BR_IMPL_CHECK_NO_OP
 `endif  // BR_ENABLE_IMPL_CHECKS
 
 // More expressive form of BR_COVER_IMPL that allows the use of custom clock and reset signal names.
@@ -126,7 +143,8 @@
 `define BR_COVER_CR_IMPL(__name__, __expr__, __clk__, __rst__) \
 `BR_COVER_CR(__name__, __expr__, __clk__, __rst__)
 `else  // BR_ENABLE_IMPL_CHECKS
-`define BR_COVER_CR_IMPL(__name__, __expr__, __clk__, __rst__) ;  // macro no-op; BR_ENABLE_IMPL_CHECKS not defined
+`define BR_COVER_CR_IMPL(__name__, __expr__, __clk__, __rst__) \
+`BR_IMPL_CHECK_NO_OP
 `endif  // BR_ENABLE_IMPL_CHECKS
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -136,14 +154,16 @@
 `define BR_COVER_COMB_INTG(__name__, __expr__) \
 `BR_COVER_COMB(__name__, __expr__)
 `else  // BR_DISABLE_INTG_CHECKS
-`define BR_COVER_COMB_INTG(__name__, __expr__) ;  // macro no-op; BR_DISABLE_INTG_CHECKS defined
+`define BR_COVER_COMB_INTG(__name__, __expr__) \
+`BR_INTG_CHECK_NO_OP
 `endif // BR_DISABLE_INTG_CHECKS
 
 `ifdef BR_ENABLE_IMPL_CHECKS
 `define BR_COVER_COMB_IMPL(__name__, __expr__) \
 `BR_COVER_COMB(__name__, __expr__)
 `else  // BR_ENABLE_IMPL_CHECKS
-`define BR_COVER_COMB_IMPL(__name__, __expr__) ;  // macro no-op; BR_ENABLE_IMPL_CHECKS not defined
+`define BR_COVER_COMB_IMPL(__name__, __expr__) \
+`BR_IMPL_CHECK_NO_OP
 `endif  // BR_ENABLE_IMPL_CHECKS
 
 // verilog_lint: waive-stop line-length
