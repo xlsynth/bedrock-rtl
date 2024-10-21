b03782db12ac2160abf8ed2212230995f6551bc5
Mark Gottscho <mgottscho@openai.com>
2024-10-21 11:17:52 -0700

    Implementation and impl checks

--- credit/rtl/br_credit_counter.sv
+++ credit/rtl/br_credit_counter.sv
@@ -62,12 +62,79 @@ module br_credit_counter #(
   // Integration checks
   //------------------------------------------
 
+  // Parameter checks
+  `BR_ASSERT_STATIC(max_value_gte_1_a, MaxValue >= 1)
+  `BR_ASSERT_STATIC(max_change_gte_1_a, MaxChange >= 1)
+  `BR_ASSERT_STATIC(max_change_lte_max_value_a, MaxChange <= MaxValue)
+
+  // Ensure the initial value was within the valid range on the last cycle when exiting reset
+  `BR_ASSERT_COMB_INTG(initial_value_in_range_a, $fell(rst) |-> $past(initial_value) <= MaxValue)
+
+  // Ensure increments and decrements are in range
+  `BR_ASSERT_INTG(incr_in_range_a, incr_valid |-> (incr <= MaxChange))
+  `BR_ASSERT_INTG(decr_in_range_a, decr_valid |-> (decr <= MaxChange))
+
+  // Assertion-only helper logic for overflow/underflow detection
+`ifdef SV_ASSSERT_ON
+`ifndef BR_DISABLE_INTG_CHECKS
+  localparam int CalcWidth = ValueWidth + 1;
+  logic [CalcWidth-1:0] value_extended_next;
+
+  // Compute the next value with extended width
+  always_comb begin
+    value_extended_next = {1'b0, value};
+    if (incr_valid) value_extended_next = value_extended_next + incr;
+    if (decr_valid) value_extended_next = value_extended_next - decr;
+  end
+`endif  // BR_DISABLE_INTG_CHECKS
+`endif  // SV_ASSERT_ON
+
+  `BR_ASSERT_INTG(no_overflow_or_underflow_a, value_extended_next <= MaxValue)
+
+  // Ensure the credit counter goes through the full range to help ensure
+  // flow control corners are hit outside the module.
+  `BR_COVER_INTG(value_reaches_zero_c, value == 0)
+  `BR_COVER_INTG(value_reaches_max_c, value == MaxValue)
+
   //------------------------------------------
   // Implementation
   //------------------------------------------
+  logic [ValueWidth-1:0] incr_internal;
+  logic [ValueWidth-1:0] decr_internal;
+
+  assign incr_internal = incr_valid ? incr : '0;
+  assign decr_internal = decr_valid ? decr : '0;
+  assign value_next = value + incr_value - decr_value;
+
+  // Register the value with synchronous reset to initial_value
+  `BR_REGI(value, value_next, initial_value)
 
   //------------------------------------------
   // Implementation checks
   //------------------------------------------
+
+  // Reset
+  `BR_ASSERT_IMPL(reset_behavior_a, $fell(rst) |-> value == $past(initial_value))
+
+  // Value basics
+  `BR_ASSERT_IMPL(value_next_a, value_next == value_extended_next[ValueWidth-1:0])
+  `BR_ASSERT_IMPL(value_updates_a, ##1 value == $past(value_next))
+
+  // Increment corners
+  `BR_COVER_IMPL(max_incr_c, incr_valid && incr == MaxChange)
+  `BR_COVER_IMPL(min_incr_c, incr_valid && incr == 0)
+
+  // Decrement corners
+  `BR_COVER_IMPL(max_decr_c, decr_valid && decr == MaxChange)
+  `BR_COVER_IMPL(min_decr_c, decr_valid && decr == 0)
+
+  // Increment/decrement combination corner cases
+  `BR_COVER_IMPL(incr_and_decr_c, incr_valid && decr_valid)
+  `BR_ASSERT_IMPL(incr_and_no_decr_c, incr_valid && !decr_valid |-> value_next >= value)
+  `BR_ASSERT_IMPL(no_incr_and_decr_c, !incr_valid && decr_valid |-> value_next <= value)
+
+  // No-op corners
+  `BR_ASSERT_IMPL(idle_noop_a, !incr_valid && !decr_valid |-> value == value_next)
+  `BR_ASSERT_IMPL(active_noop_a, incr_valid && decr_valid && incr == decr |-> value == value_next)
 
 endmodule : br_credit_counter
