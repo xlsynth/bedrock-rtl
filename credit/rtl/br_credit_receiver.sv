0fd5e160cf7a9fa994764e25b35f1e1bb33743fd
Mark Gottscho <mgottscho@openai.com>
2024-10-23 16:46:36 +0000

    add withhold_credit to br_credit_receiver

--- credit/rtl/br_credit_receiver.sv
+++ credit/rtl/br_credit_receiver.sv
@@ -51,9 +51,6 @@ module br_credit_receiver #(
     // Synchronous active-high reset.
     input logic rst,
 
-    // Reset value for the credit counter
-    input logic [CounterWidth-1:0] initial_credit,
-
     // Credit/valid push interface.
     input logic push_credit_stall,
     output logic push_credit,
@@ -68,6 +65,10 @@ module br_credit_receiver #(
     output logic pop_valid,
     output logic [BitWidth-1:0] pop_data,
 
+    // Reset value for the credit counter
+    input  logic [CounterWidth-1:0] initial_credit,
+    // Dynamically withhold credits from circulation
+    input  logic [CounterWidth-1:0] withhold_credit,
     // Credit counter status
     output logic [CounterWidth-1:0] credit_count,
     output logic [CounterWidth-1:0] credit_count_next
@@ -80,12 +81,17 @@ module br_credit_receiver #(
   `BR_ASSERT_STATIC(max_credit_in_range_a, MaxCredit >= 1)
 
   `BR_ASSERT_INTG(no_push_valid_if_no_credit_released_a, credit_count == MaxCredit |-> !push_valid)
+  `BR_ASSERT_INTG(withhold_credit_in_range_a, withhold_credit <= MaxCredit)
+  `BR_COVER_INTG(withhold_credit_c, withhold_credit > 0)
 
   // Rely on submodule integration checks
 
   //------------------------------------------
   // Implementation
   //------------------------------------------
+  logic [CounterWidth-1:0] credit_count_plus_pop_credit;
+  logic [CounterWidth-1:0] available_credit;
+
   br_credit_counter #(
       .MaxValue (MaxCredit),
       .MaxChange(1)
@@ -101,7 +107,11 @@ module br_credit_receiver #(
       .value_next(credit_count_next)
   );
 
-  assign push_credit = !push_credit_stall && ((credit_count > 0) || pop_credit);
+  assign credit_count_plus_pop_credit = credit_count + pop_credit;
+  assign available_credit = credit_count_plus_pop_credit > withhold_credit ?
+    credit_count_plus_pop_credit - withhold_credit :
+    '0;
+  assign push_credit = !push_credit_stall && (available_credit > 0);
   assign pop_valid = push_valid;
   assign pop_data = push_data;
 
@@ -111,6 +121,9 @@ module br_credit_receiver #(
   `BR_ASSERT_IMPL(push_credit_stall_a, push_credit_stall |-> !push_credit)
   `BR_COVER_IMPL(passthru_credit_c, pop_credit && push_credit && credit_count == '0)
   `BR_COVER_IMPL(passthru_credit_nonzero_count_c, pop_credit && push_credit && credit_count > '0)
+  `BR_ASSERT_IMPL(over_withhold_a, withhold_credit > credit_count |-> !push_credit)
+  `BR_COVER_IMPL(withhold_and_release_c,
+                 credit_count == withhold_credit && push_credit |-> pop_credit)
 
   // Rely on submodule implementation checks
 
