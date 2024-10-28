c2f15e4473d4a6f35a89ddd1c14a49f181b3d635
Mark Gottscho <mgottscho@openai.com>
2024-10-28 18:19:20 +0000

    fix silly cover again

--- fifo/rtl/internal/br_fifo_push_ctrl_credit.sv
+++ fifo/rtl/internal/br_fifo_push_ctrl_credit.sv
@@ -72,7 +72,7 @@ module br_fifo_push_ctrl_credit #(
   `BR_ASSERT_STATIC(bit_width_must_be_at_least_one_a, BitWidth >= 1)
   `BR_ASSERT_STATIC(credit_width_a, CreditWidth >= $clog2(Depth + 1))
 
-  `BR_ASSERT_INTG(full_c, full)
+  `BR_COVER_INTG(full_c, full)
 
   // Internal integration checks
 
