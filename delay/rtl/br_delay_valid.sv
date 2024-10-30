ffe2b746fbc4f460a4d0ba29a6e7b6c6e507df48
Trevor Bunker <tbunker@openai.com>
2024-10-30 14:06:49 -0700

    Remove trailing space

--- delay/rtl/br_delay_valid.sv
+++ delay/rtl/br_delay_valid.sv
@@ -39,7 +39,7 @@ module br_delay_valid #(
     // Output of last delay stage (delayed by NumStages cycles).
     output logic [BitWidth-1:0]               out,
     // Indicates the valid status of each delay stage.
-    output logic [ NumStages:0]               out_valid_stages, 
+    output logic [ NumStages:0]               out_valid_stages,
     // Output of each delay stage. Note that out_stages[0] == in, and
     // out_stages[NumStages] == out.
     output logic [ NumStages:0][BitWidth-1:0] out_stages
