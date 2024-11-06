9708955edbefd85f75a587f582ebb4954668c12f
Mark Gottscho <mgottscho@openai.com>
2024-11-06 09:35:09 -0800

    Add __name__ to macro

--- macros/br_unused.svh
+++ macros/br_unused.svh
@@ -19,7 +19,7 @@
 // verilog_lint: waive-start line-length
 // verilog_format: off
 
-`define BR_UNUSED(__x__) br_misc_unused #(.BitWidth($bits(__x__))) br_misc_unused__``__x__(.in(__x__));
+`define BR_UNUSED(__name__, __x__) br_misc_unused #(.BitWidth($bits(__x__))) br_misc_unused_``__name__(.in(__x__));
 
 // ri lint_check_on LINE_LENGTH
 // verilog_lint: waive-stop line-length
