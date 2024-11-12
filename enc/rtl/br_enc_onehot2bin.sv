04a991d339250cdc156d6238d541d02465681819
Matt Lee <mattskl@openai.com>
2024-11-12 23:51:41 +0000

    Add output valid signal to onehot2bin module.

--- enc/rtl/br_enc_onehot2bin.sv
+++ enc/rtl/br_enc_onehot2bin.sv
@@ -53,9 +53,9 @@ module br_enc_onehot2bin #(
     parameter int BinWidth = $clog2(NumValues)
 ) (
     // ri lint_check_waive INPUT_NOT_READ HIER_NET_NOT_READ HIER_BRANCH_NOT_READ
-    input logic                  clk,  // Used only for assertions
+    input logic                  clk,        // Used only for assertions
     // ri lint_check_waive INPUT_NOT_READ HIER_NET_NOT_READ HIER_BRANCH_NOT_READ
-    input logic                  rst,  // Used only for assertions
+    input logic                  rst,        // Used only for assertions
     // in[0] is not used and does not impact the output
     // because it is "don't care" if all other bits are 0.
     // ri lint_check_waive INEFFECTIVE_NET
