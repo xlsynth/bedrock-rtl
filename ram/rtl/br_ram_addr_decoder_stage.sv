878b1528ec71e3e319c9e70492d8b15a0202ba3b
Mark Gottscho <mgottscho@openai.com>
2024-11-13 05:31:42 +0000

    support 0-latency decoder stage

--- ram/rtl/br_ram_addr_decoder_stage.sv
+++ ram/rtl/br_ram_addr_decoder_stage.sv
@@ -18,26 +18,31 @@
 
 module br_ram_addr_decoder_stage #(
     parameter int InputAddressWidth = 2,  // Width of the address. Must be at least 2.
-    parameter int NumForks = 1,  // Number of output forks. Must be a positive power-of-2.
-    localparam int ForkSelectWidth = $clog2(NumForks),  // Must be less than InputAddressWidth.
+    parameter int Forks = 1,  // Number of output forks. Must be a positive power-of-2.
+    parameter bit RegisterOutputs = 0,  // If 1, then pipeline latency is 1 cycle; else, 0 cycles.
+    localparam int ForkSelectWidth = $clog2(Forks),  // Must be less than InputAddressWidth.
     localparam int OutputAddressWidth = InputAddressWidth - ForkSelectWidth
 ) (
     // Posedge-triggered clock.
+    // Can be unused if RegisterOutputs == 0.
+    // ri lint_check_waive HIER_NET_NOT_READ HIER_BRANCH_NOT_READ
     input  logic                                                 clk,
     // Synchronous active-high reset.
+    // Can be unused if RegisterOutputs == 0.
+    // ri lint_check_waive HIER_NET_NOT_READ HIER_BRANCH_NOT_READ
     input  logic                                                 rst,
     input  logic                                                 in_addr_valid,
     input  logic [InputAddressWidth-1:0]                         in_addr,
-    output logic [         NumForks-1:0]                         out_addr_valid,
-    output logic [         NumForks-1:0][OutputAddressWidth-1:0] out_addr
+    output logic [            Forks-1:0]                         out_addr_valid,
+    output logic [            Forks-1:0][OutputAddressWidth-1:0] out_addr
 );
 
   //------------------------------------------
   // Integration checks
   //------------------------------------------
   `BR_ASSERT_STATIC(input_address_width_gte2_a, InputAddressWidth >= 2)
-  `BR_ASSERT_STATIC(forks_gte1_a, NumForks >= 1)
-  `BR_ASSERT_STATIC(forks_power_of_2_a, br_math::is_power_of_2(NumForks))
+  `BR_ASSERT_STATIC(forks_gte1_a, Forks >= 1)
+  `BR_ASSERT_STATIC(forks_power_of_2_a, br_math::is_power_of_2(Forks))
   `BR_ASSERT_STATIC(fork_select_width_correct_a, ForkSelectWidth < InputAddressWidth)
 
   // TODO(mgottscho): write more
@@ -47,12 +52,12 @@ module br_ram_addr_decoder_stage #(
   //------------------------------------------
 
   // Base case: single fork, i.e., just a simple delay register
-  if (NumForks == 1) begin : gen_fork_eq_1
+  if (Forks == 1) begin : gen_fork_eq_1
     `BR_ASSERT_STATIC(output_address_width_correct_a, OutputAddressWidth == InputAddressWidth)
 
     br_delay_valid #(
         .BitWidth (OutputAddressWidth),
-        .NumStages(1)
+        .NumStages(RegisterOutputs ? 1 : 0)
     ) br_delay_valid (
         .clk,
         .rst,
@@ -73,15 +78,15 @@ module br_ram_addr_decoder_stage #(
 
     `BR_ASSERT_STATIC(select_check_a, SelectMsb >= SelectLsb)
 
-    logic [NumForks-1:0] fork_valid;
+    logic [Forks-1:0] fork_valid;
 
     // Replicate output registers to reduce fanout on each register
-    for (genvar i = 0; i < NumForks; i++) begin : gen_fork
+    for (genvar i = 0; i < Forks; i++) begin : gen_fork
       assign fork_valid[i] = in_addr_valid && (in_addr[SelectMsb:SelectLsb] == i);
 
       br_delay_valid #(
           .BitWidth (OutputAddressWidth),
-          .NumStages(1)
+          .NumStages(RegisterOutputs ? 1 : 0)
       ) br_delay_valid (
           .clk,
           .rst,
@@ -98,15 +103,25 @@ module br_ram_addr_decoder_stage #(
   //------------------------------------------
   // Implementation checks
   //------------------------------------------
-  `BR_ASSERT_IMPL(valid_propagation_a, in_addr_valid |=> $onehot(out_addr_valid))
   `BR_ASSERT_IMPL(out_addr_valid_onehot0_a, $onehot0(out_addr_valid))
-  for (genvar i = 0; i < NumForks; i++) begin : gen_fork_check
-    `BR_ASSERT_IMPL(out_addr_correct_a,
-                    out_addr_valid[i] |-> $past(
-                        in_addr_valid
-                    ) && (out_addr[i] == $past(
-                        in_addr[OutputAddressWidth-1:0]
-                    )))
+
+  if (RegisterOutputs) begin : gen_impl_checks_registered
+    `BR_ASSERT_IMPL(valid_propagation_a, in_addr_valid |=> $onehot(out_addr_valid))
+    for (genvar i = 0; i < Forks; i++) begin : gen_fork_check
+      `BR_ASSERT_IMPL(out_addr_correct_a,
+                      out_addr_valid[i] |-> $past(
+                          in_addr_valid
+                      ) && (out_addr[i] == $past(
+                          in_addr[OutputAddressWidth-1:0]
+                      )))
+    end
+  end else begin : gen_impl_checks_not_registered
+    `BR_ASSERT_IMPL(valid_propagation_a, in_addr_valid |-> $onehot(out_addr_valid))
+    for (genvar i = 0; i < Forks; i++) begin : gen_fork_check
+      `BR_ASSERT_IMPL(
+          out_addr_correct_a,
+          out_addr_valid[i] |-> in_addr_valid && (out_addr[i] == in_addr[OutputAddressWidth-1:0]))
+    end
   end
 
 endmodule : br_ram_addr_decoder_stage
