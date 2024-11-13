f801bd3f1f6f705e831b7948b8902bd8616f2b60
Mark Gottscho <mgottscho@openai.com>
2024-11-13 09:19:10 -0800

    fixes

--- ram/rtl/br_ram_addr_decoder_stage.sv
+++ ram/rtl/br_ram_addr_decoder_stage.sv
@@ -70,9 +70,9 @@ module br_ram_addr_decoder_stage #(
     ) br_delay_valid (
         .clk,
         .rst,
-        .in_valid(in_addr_valid),
+        .in_valid(in_valid),
         .in({in_addr, in_data}),
-        .out_valid(out_addr_valid),
+        .out_valid(out_valid),
         .out({out_addr, out_data}),
         .out_valid_stages(),  // unused
         .out_stages()  // unused
@@ -91,7 +91,7 @@ module br_ram_addr_decoder_stage #(
 
     // Replicate output registers to reduce fanout on each register
     for (genvar i = 0; i < Forks; i++) begin : gen_fork
-      assign fork_valid[i] = in_addr_valid && (in_addr[SelectMsb:SelectLsb] == i);
+      assign fork_valid[i] = in_valid && (in_addr[SelectMsb:SelectLsb] == i);
 
       br_delay_valid #(
           .BitWidth (OutputAddressWidth + DataWidth),
@@ -101,7 +101,7 @@ module br_ram_addr_decoder_stage #(
           .rst,
           .in_valid(fork_valid[i]),
           .in({in_addr[OutputAddressWidth-1:0], in_data}),
-          .out_valid(out_addr_valid[i]),
+          .out_valid(out_valid[i]),
           .out({out_addr[i], out_data}),
           .out_valid_stages(),  // unused
           .out_stages()  // unused
@@ -112,24 +112,23 @@ module br_ram_addr_decoder_stage #(
   //------------------------------------------
   // Implementation checks
   //------------------------------------------
-  `BR_ASSERT_IMPL(out_addr_valid_onehot0_a, $onehot0(out_addr_valid))
+  `BR_ASSERT_IMPL(out_valid_onehot0_a, $onehot0(out_valid))
 
   if (RegisterOutputs) begin : gen_impl_checks_registered
-    `BR_ASSERT_IMPL(valid_propagation_a, in_addr_valid |=> $onehot(out_addr_valid))
+    `BR_ASSERT_IMPL(valid_propagation_a, in_valid |=> $onehot(out_valid))
     for (genvar i = 0; i < Forks; i++) begin : gen_fork_check
       `BR_ASSERT_IMPL(out_addr_correct_a,
-                      out_addr_valid[i] |-> $past(
-                          in_addr_valid
+                      out_valid[i] |-> $past(
+                          in_valid
                       ) && (out_addr[i] == $past(
                           in_addr[OutputAddressWidth-1:0]
                       )))
     end
   end else begin : gen_impl_checks_not_registered
-    `BR_ASSERT_IMPL(valid_propagation_a, in_addr_valid |-> $onehot(out_addr_valid))
+    `BR_ASSERT_IMPL(valid_propagation_a, in_valid |-> $onehot(out_valid))
     for (genvar i = 0; i < Forks; i++) begin : gen_fork_check
-      `BR_ASSERT_IMPL(
-          out_addr_correct_a,
-          out_addr_valid[i] |-> in_addr_valid && (out_addr[i] == in_addr[OutputAddressWidth-1:0]))
+      `BR_ASSERT_IMPL(out_addr_correct_a,
+                      out_valid[i] |-> in_valid && (out_addr[i] == in_addr[OutputAddressWidth-1:0]))
     end
   end
 
