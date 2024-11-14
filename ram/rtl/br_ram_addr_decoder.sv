48666b44ae7c973d332143fd9f488fb0d98ae2ab
Mark Gottscho <mgottscho@openai.com>
2024-11-14 21:44:46 +0000

    fixes

--- ram/rtl/br_ram_addr_decoder.sv
+++ ram/rtl/br_ram_addr_decoder.sv
@@ -12,30 +12,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Bedrock-RTL Address Decoder Stage
+// Bedrock-RTL Address Decoder
 //
 // Decodes and steers an input address and data to one output tile based on the
 // most-significant bits of the address.
 //
-// The number of tiles must be a positive power-of-2.
+// Works for any RAM depth >= 2 and any number of output tiles >= 1 that evenly
+// divides the RAM depth. If RAM depth is a power-of-2 then the implementation is optimized.
 //
 // The latency is 0 cycles if RegisterOutputs is 0; otherwise, it is 1 cycle (pipelined).
 
 `include "br_asserts_internal.svh"
 
-module br_ram_addr_decoder_stage #(
-    // Width of the address. Must be at least 1.
-    parameter int InputAddressWidth = 1,
+module br_ram_addr_decoder #(
+    // Depth of the RAM. Must be at least 2.
+    parameter int Depth = 2,
     // Sideband signals to pipeline in lockstep with the address decoding.
     // Safe to tie-off if not used. Must be at least 1.
     parameter int DataWidth = 1,
-    // Number of output tiles. Must be a positive power-of-2 and less than
-    // or equal to InputAddressWidth.
+    // Number of output address tiles. Must be at least 1 and evenly divide Depth.
     parameter int Tiles = 1,
     // If 1, then pipeline latency is 1 cycle; else, 0 cycles.
     parameter bit RegisterOutputs = 0,
-    localparam int TileSelectWidth = $clog2(Tiles),
-    localparam int OutputAddressWidth = InputAddressWidth - TileSelectWidth
+    localparam int TileDepth = br_math::ceil_div(Depth, Tiles),
+    localparam int InputAddressWidth = $clog2(Depth),
+    localparam int OutputAddressWidth = $clog2(TileDepth)
 ) (
     // Posedge-triggered clock.
     // Can be unused if RegisterOutputs == 0.
@@ -58,17 +59,19 @@ module br_ram_addr_decoder_stage #(
   //------------------------------------------
   // Integration checks
   //------------------------------------------
-  `BR_ASSERT_STATIC(input_address_width_gte1_a, InputAddressWidth >= 1)
+  `BR_ASSERT_STATIC(depth_gte1_a, Depth >= 2)
+  `BR_ASSERT_STATIC(data_width_gte1_a, DataWidth >= 1)
   `BR_ASSERT_STATIC(tiles_gte1_a, Tiles >= 1)
-  `BR_ASSERT_STATIC(tiles_power_of_2_a, br_math::is_power_of_2(Tiles))
-  `BR_ASSERT_STATIC(tiles_lte_input_address_width_a, Tiles <= InputAddressWidth)
+  `BR_ASSERT_STATIC(tiles_evenly_divides_depth_a, (Tiles * TileDepth) == Depth)
+
+  `BR_ASSERT_INTG(in_addr_in_range_a, in_valid |-> in_addr < Depth)
 
   //------------------------------------------
   // Implementation
   //------------------------------------------
 
   // Base case: single tile, i.e., just a simple delay register
-  if (Tiles == 1) begin : gen_tile1
+  if (Tiles == 1) begin : gen_tiles_eq1
     `BR_ASSERT_STATIC(output_address_width_ok_a, OutputAddressWidth == InputAddressWidth)
 
     br_delay_valid #(
@@ -85,29 +88,51 @@ module br_ram_addr_decoder_stage #(
         .out_stages()  // unused
     );
 
-    // Common case: multiple fanout, i.e., requires decoding to one of them (replicated delay registers)
-  end else begin : gen_fanout_gt_1
+    // Common case: multiple tiles, i.e., requires decoding to one of them (replicated delay registers)
+  end else begin : gen_tiles_gt1
     `BR_ASSERT_STATIC(output_address_width_ok_a, OutputAddressWidth < InputAddressWidth)
-
-    localparam int SelectMsb = InputAddressWidth - 1;
-    localparam int SelectLsb = (SelectMsb - TileSelectWidth) + 1;
-
-    `BR_ASSERT_STATIC(select_check_a, SelectMsb >= SelectLsb)
 
     logic [Tiles-1:0]                         internal_out_valid;
     logic [Tiles-1:0][OutputAddressWidth-1:0] internal_out_addr;
     logic [Tiles-1:0][         DataWidth-1:0] internal_out_data;
 
-    br_demux_bin #(
-        .NumSymbolsOut(Tiles),
-        .SymbolWidth  (OutputAddressWidth + DataWidth)
-    ) br_demux_bin (
-        .select(in_addr[SelectMsb:SelectLsb]),
-        .in_valid(in_valid),
-        .in({in_addr[OutputAddressWidth-1:0], in_data}),
-        .out_valid(internal_out_valid),
-        .out({internal_out_addr, internal_out_data})
-    );
+    // If Depth is a power of 2 (and Tiles evenly divides it), then we know we can
+    // decode the address by looking only at the MSBs as the tile select bits,
+    // and simply slice them off.
+    if (br_math::is_power_of_2(Depth)) begin : gen_demux_impl
+      localparam int TileSelectWidth = $clog2(Tiles);
+      localparam int SelectMsb = InputAddressWidth - 1;
+      localparam int SelectLsb = (SelectMsb - TileSelectWidth) + 1;
+      `BR_ASSERT_STATIC(select_check_a, SelectMsb >= SelectLsb)
+
+      br_demux_bin #(
+          .NumSymbolsOut(Tiles),
+          .SymbolWidth  (OutputAddressWidth + DataWidth)
+      ) br_demux_bin (
+          .select(in_addr[SelectMsb:SelectLsb]),
+          .in_valid(in_valid),
+          .in({in_addr[OutputAddressWidth-1:0], in_data}),
+          .out_valid(internal_out_valid),
+          .out({internal_out_addr, internal_out_data})
+      );
+
+      // If Depth is not a power-of-2 we cannot just slice off the MSBs for the tile select.
+      // We have to look at the address range and steer it with bit overlaps.
+    end else begin : gen_compare_impl
+      for (genvar i = 0; i < Tiles; i++) begin : gen_compare
+        // inclusive
+        localparam logic [InputAddressWidth-1:0] TileBaseAddress = TileDepth * i;
+        // exclusive
+        localparam logic [InputAddressWidth-1:0] TileBoundAddress = TileDepth * (i + 1);
+
+        assign internal_out_valid[i] = in_valid &&
+            // Lint waiver needed because when i == 0, this subexpression is always true.
+            // ri lint_check_waive INVALID_COMPARE
+            (in_addr >= TileBaseAddress) && (in_addr < TileBoundAddress);
+        assign internal_out_addr[i] = in_addr[OutputAddressWidth-1:0];
+        assign internal_out_data[i] = in_data;
+      end
+    end
 
     // Replicate to reduce register fanout when RegisterOutputs == 1
     for (genvar i = 0; i < Tiles; i++) begin : gen_out
@@ -131,6 +156,9 @@ module br_ram_addr_decoder_stage #(
   // Implementation checks
   //------------------------------------------
   `BR_ASSERT_IMPL(out_valid_onehot0_a, $onehot0(out_valid))
+  for (genvar i = 0; i < Tiles; i++) begin : gen_out_addr_checks
+    `BR_ASSERT_IMPL(out_addr_in_range_a, out_valid[i] |-> out_addr[i] < TileDepth)
+  end
 
   if (RegisterOutputs) begin : gen_impl_checks_registered
     `BR_ASSERT_IMPL(valid_propagation_a, in_valid |=> $onehot(out_valid))
@@ -150,4 +178,4 @@ module br_ram_addr_decoder_stage #(
     end
   end
 
-endmodule : br_ram_addr_decoder_stage
+endmodule : br_ram_addr_decoder
