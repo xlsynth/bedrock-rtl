51b8016e988f676c3ea55303a5d33daa01fb230a
Oliver Bowen <obowen@openai.com>
2024-11-20 15:01:52 -0800

    Fix assertions

--- delay/rtl/br_delay_shift_reg.sv
+++ delay/rtl/br_delay_shift_reg.sv
@@ -66,11 +66,11 @@ module br_delay_shift_reg #(
   // Implementation checks
   //------------------------------------------
   if (NumStages == 1) begin : gen_assert_one_stage
-    `BR_ASSERT_IMPL(value_shifted_a, (shift_en && !reinit) |=> value == shift_in)
+    `BR_ASSERT_IMPL(value_shifted_a, (shift_en && !reinit) |=> value == $past(shift_in))
   end else begin : gen_assert_multi_stage
-    `BR_ASSERT_IMPL(value_shifted_a, (shift_en && !reinit) |=> value == {stages, shift_in})
+    `BR_ASSERT_IMPL(value_shifted_a, (shift_en && !reinit) |=> value == $past({stages, shift_in}))
   end
-  `BR_ASSERT_IMPL(value_initialized_a, reinit |=> value == initial_value)
+  `BR_ASSERT_IMPL(value_initialized_a, reinit |=> value == $past(initial_value))
   `BR_ASSERT_IMPL(value_stable_a, (!shift_en && !reinit) |=> $stable(value))
 
 endmodule : br_delay_shift_reg
