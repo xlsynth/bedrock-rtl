xlsynth/bedrock-rtl

commit 526b94deb5affbcc274e6ca19cc0d66788426939

dont allow push_credit to assert during reset (#304)
Fixes #303 

cc: @masai-openai @taoliu-o @zhemao-openai

credit/rtl/br_credit_receiver.sv
@@ -138,7 +138,9 @@ module br_credit_receiver #(
   if (RegisterPushCredit) begin : gen_reg_push
     `BR_REG(push_credit, push_credit_internal)
   end else begin : gen_passthru_push
-    assign push_credit = push_credit_internal;
+    logic reset_released;
+    `BR_REG(reset_released, 1'b1)
+    assign push_credit = reset_released && push_credit_internal;
   end
 
   //------------------------------------------

commit b2fc0918e10531becb708f368cd730a49fa75a4b

stall credit release until flopped reset deassert (#314)
cc: @masai-openai 

Fixes bug introduced in #304 that causes the very first credit released
after reset to be lost.

credit/rtl/br_credit_receiver.sv
@@ -130,17 +130,18 @@ module br_credit_receiver #(
       .available(credit_available)
   );
 
-  assign credit_decr_valid = !push_credit_stall;
   assign push_credit_internal = credit_decr_valid && credit_decr_ready;
   assign pop_valid = push_valid;
   assign pop_data = push_data;
 
   if (RegisterPushCredit) begin : gen_reg_push
     `BR_REG(push_credit, push_credit_internal)
+    assign credit_decr_valid = !push_credit_stall;
   end else begin : gen_passthru_push
     logic reset_released;
     `BR_REG(reset_released, 1'b1)
-    assign push_credit = reset_released && push_credit_internal;
+    assign credit_decr_valid = !push_credit_stall && reset_released;
+    assign push_credit = push_credit_internal;
   end
 
   //------------------------------------------