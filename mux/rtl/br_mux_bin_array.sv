a79ecd4751e5a405dd46071a461c2a84d86ddb9e
Trevor Bunker <tbunker@openai.com>
2025-01-13 18:01:13 +0000

    update based on comments

--- mux/rtl/br_mux_bin_array.sv
+++ mux/rtl/br_mux_bin_array.sv
@@ -12,17 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-// Bedrock-RTL Multiplexer Array
+// Bedrock-RTL Binary-Select Multiplexer Array
 //
-// An array of N-to-1 multiplexers with a select.
+// An array of N-to-1 multiplexers with a binary select.
 //
-// The out[j] signal is set to in[i] for which select == i.
-// Select must be in range of NumSymbolsIn.
+// The out[j] signal is set to in[i] for which select[j] == i.
+// Each select must be in range of NumSymbolsIn.
 
 `include "br_asserts_internal.svh"
 `include "br_unused.svh"
 
-module br_mux_array #(
+module br_mux_bin_array #(
     // Number of inputs to select among. Must be >= 2.
     parameter  int NumSymbolsIn  = 2,
     // Number of outputs to select among. Must be >= 1.
@@ -31,9 +31,10 @@ module br_mux_array #(
     parameter  int SymbolWidth   = 1,
     localparam int SelectWidth   = $clog2(NumSymbolsIn)
 ) (
-    input  logic [NumSymbolsOut-1:0][SelectWidth-1:0] select,
-    input  logic [ NumSymbolsIn-1:0][SymbolWidth-1:0] in,
-    output logic [NumSymbolsOut-1:0][SymbolWidth-1:0] out
+    input logic [NumSymbolsOut-1:0][SelectWidth-1:0] select,
+    input logic [NumSymbolsIn-1:0][SymbolWidth-1:0] in,
+    output logic [NumSymbolsOut-1:0][SymbolWidth-1:0] out,
+    output logic [NumSymbolsOut-1:0] out_valid
 );
 
   //------------------------------------------
@@ -43,24 +44,19 @@ module br_mux_array #(
   `BR_ASSERT_STATIC(legal_num_symbols_out_a, NumSymbolsOut >= 1)
   `BR_ASSERT_STATIC(legal_symbol_width_a, SymbolWidth >= 1)
 
-  // ri lint_check_waive ALWAYS_COMB
-  `BR_ASSERT_COMB_INTG(select_in_range_a, $isunknown(select) || select < NumSymbolsIn)
-
   //------------------------------------------
   // Implementation
   //------------------------------------------
-  generate
-    for (genvar i = 0; i < NumSymbolsOut; i++) begin : gen_mux
-      br_mux_bin #(
-          .NumSymbolsIn(NumSymbolsIn),
-          .SymbolWidth (SymbolWidth)
-      ) br_mux_bin (
-          .select(select[i]),
-          .in(in),
-          .out(out[i])
-      );
-    end
-  endgenerate
+  for (genvar i = 0; i < NumSymbolsOut; i++) begin : gen_mux
+    br_mux_bin #(
+        .NumSymbolsIn(NumSymbolsIn),
+        .SymbolWidth (SymbolWidth)
+    ) br_mux_bin (
+        .select(select[i]),
+        .in(in),
+        .out(out[i]),
+        .out_valid(out_valid[i])
+    );
+  end
 
-
-endmodule : br_mux_array
+endmodule : br_mux_bin_array
