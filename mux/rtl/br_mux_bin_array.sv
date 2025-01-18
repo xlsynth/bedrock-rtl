b402d50b6a799fd867aba87b4f4f666db69dfa96
Trevor Bunker <tbunker@openai.com>
2025-01-17 21:11:17 -0800

    allow mux array to use structured gates (#332)

--- mux/rtl/br_mux_bin_array.sv
+++ mux/rtl/br_mux_bin_array.sv
@@ -24,12 +24,16 @@
 
 module br_mux_bin_array #(
     // Number of inputs to select among. Must be >= 2.
-    parameter  int NumSymbolsIn  = 2,
+    parameter int NumSymbolsIn = 2,
     // Number of outputs to select among. Must be >= 1.
-    parameter  int NumSymbolsOut = 1,
+    parameter int NumSymbolsOut = 1,
     // The width of each symbol in bits. Must be >= 1.
-    parameter  int SymbolWidth   = 1,
-    localparam int SelectWidth   = $clog2(NumSymbolsIn)
+    parameter int SymbolWidth = 1,
+    // If set to 1, manually build a tree of mux2 gates instead of relying on
+    // the synthesis tool.  This may be necessary if implementing an
+    // asynchronous path.
+    parameter bit UseStructuredGates = 0,
+    localparam int SelectWidth = $clog2(NumSymbolsIn)
 ) (
     input logic [NumSymbolsOut-1:0][SelectWidth-1:0] select,
     input logic [NumSymbolsIn-1:0][SymbolWidth-1:0] in,
@@ -50,7 +54,8 @@ module br_mux_bin_array #(
   for (genvar i = 0; i < NumSymbolsOut; i++) begin : gen_mux
     br_mux_bin #(
         .NumSymbolsIn(NumSymbolsIn),
-        .SymbolWidth (SymbolWidth)
+        .SymbolWidth(SymbolWidth),
+        .UseStructuredGates(UseStructuredGates)
     ) br_mux_bin (
         .select(select[i]),
         .in(in),
