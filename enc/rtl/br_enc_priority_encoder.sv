6b2be69b3ef0580ea2abffd0e90913113e4826ba
Howard Mao <zhemao@openai.com>
2025-02-05 18:02:09 -0800

    Add parameter to br_enc_priority to allow the priority to be reversed (#419)
    **Stack**:
    - #422
    - #421
    - #420
    - #419 ⬅
    
    
    ⚠️ *Part of a stack created by [spr](https://github.com/ejoffe/spr). Do
    not merge manually using the UI - doing so may have unexpected results.*

--- enc/rtl/br_enc_priority_encoder.sv
+++ enc/rtl/br_enc_priority_encoder.sv
@@ -14,14 +14,13 @@
 
 // Bedrock-RTL Priority Encoder
 //
-// Masks all but the highest priority active request in an input where the
-// lowest index is the highest priority.
+// Masks all but the highest priority active request in an input.
+// This can be either the LSB or the MSB depending on the setting of
+// the MsbHighestPriority parameter.
 //
 // Can optionally produce more than one result if NumResults is greater than 1.
 // If so, out[0] will have the highest priority bit set, out[1] will have the
 // second highest priority bit set, etc.
-
-// TODO(mgottscho): Write spec
 
 `include "br_asserts_internal.svh"
 `include "br_unused.svh"
@@ -30,7 +29,10 @@ module br_enc_priority_encoder #(
     // Must be at least 2 and greater than NumResults.
     parameter int NumRequesters = 2,
     // Number of onehot results to produce. Must be at least 1.
-    parameter int NumResults = 1
+    parameter int NumResults = 1,
+    // If 1, in[NumRequesters-1] is the highest priority bit.
+    // If 0, in[0] is the highest priority bit.
+    parameter bit MsbHighestPriority = 0
 ) (
     // ri lint_check_waive INPUT_NOT_READ HIER_NET_NOT_READ HIER_BRANCH_NOT_READ
     input logic clk,  // Used only for assertions
@@ -64,22 +66,36 @@ module br_enc_priority_encoder #(
   //------------------------------------------
   // Implementation
   //------------------------------------------
+  // Internal versions of in and out such that LSB
+  // is the highest priority bit.
+  logic [NumRequesters-1:0] in_internal;
+  logic [NumResults-1:0][NumRequesters-1:0] out_internal;
+
+  if (MsbHighestPriority) begin : gen_msb_first
+    // Reverse the order of the input so that in_internal
+    // can be treated as LSB having the highest priority.
+    for (genvar i = 0; i < NumRequesters; i++) begin : gen_in
+      assign in_internal[i] = in[NumRequesters-i-1];
+    end
+  end else begin : gen_lsb_first
+    assign in_internal = in;
+  end
 
   // Each result is a separate priority search on the input
   // with the bits selected by previous results masked off.
-  for (genvar out_idx = 0; out_idx < NumResults; out_idx++) begin : gen_out
+  for (genvar out_idx = 0; out_idx < NumResults; out_idx++) begin : gen_out_internal
     logic [NumRequesters-1:0] in_masked;
 
     // Generated the masked input
     // Any bit that was set in a previous result
     // should be cleared in the masked input.
     always_comb begin
-      in_masked = in;
+      in_masked = in_internal;
       // This for loop won't be entered if out_idx is 0
       // This is expected since we just want in_masked to be in
       // ri lint_check_waive LOOP_NOT_ENTERED
       for (int prev_idx = 0; prev_idx < out_idx; prev_idx++) begin
-        in_masked &= ~out[prev_idx];
+        in_masked &= ~out_internal[prev_idx];
       end
     end
 
@@ -88,7 +104,7 @@ module br_enc_priority_encoder #(
     // out[out_idx][out_idx-1:0] must always be zero
     if (out_idx > 0) begin : gen_out_masked_lsb_tieoff
       // ri lint_check_waive CONST_OUTPUT
-      assign out[out_idx][out_idx-1:0] = '0;
+      assign out_internal[out_idx][out_idx-1:0] = '0;
       `BR_UNUSED(in_masked[out_idx-1:0])
       `BR_ASSERT_IMPL(in_masked_lsb_zero_a, in_masked[out_idx-1:0] == '0)
     end
@@ -96,12 +112,25 @@ module br_enc_priority_encoder #(
     // Generate the onehot result for this stage
     // LSB is highest priority and is set unconditionally
     // if the masked input bit is set.
-    assign out[out_idx][out_idx] = in_masked[out_idx];
+    assign out_internal[out_idx][out_idx] = in_masked[out_idx];
     for (genvar in_idx = out_idx + 1; in_idx < NumRequesters; in_idx++) begin : gen_out_bit
       // Output is set if the corresponding input bit is set and all lower index
       // bits of the masked input are not set.
-      assign out[out_idx][in_idx] = in_masked[in_idx] && (in_masked[in_idx-1:out_idx] == '0);
+      assign out_internal[out_idx][in_idx] =
+          in_masked[in_idx] && (in_masked[in_idx-1:out_idx] == '0);
     end
+  end
+
+  if (MsbHighestPriority) begin : gen_msb_first_out
+    // Since we reversed the input, we need to reverse it back
+    // for the output.
+    for (genvar i = 0; i < NumResults; i++) begin : gen_out
+      for (genvar j = 0; j < NumRequesters; j++) begin : gen_out_bit
+        assign out[i][j] = out_internal[i][NumRequesters-j-1];
+      end
+    end
+  end else begin : gen_lsb_first_out
+    assign out = out_internal;
   end
 
   //------------------------------------------
@@ -121,8 +150,8 @@ module br_enc_priority_encoder #(
       num_prev_out[out_idx] = '0;
       // ri lint_check_waive LOOP_NOT_ENTERED
       for (int i = 0; i < out_idx; i++) begin
-        prev_out[out_idx] |= out[i];
-        num_prev_out[out_idx] += |out[i];
+        prev_out[out_idx] |= out_internal[i];
+        num_prev_out[out_idx] += |out_internal[i];
       end
     end
 
@@ -130,23 +159,25 @@ module br_enc_priority_encoder #(
     `BR_ASSERT_IMPL(out_onehot0_a, $onehot0(out[out_idx]))
     // The highest priority bit of the output must be set if the corresponding
     // input bit is set and it was not taken by a previous result.
-    `BR_ASSERT_IMPL(out_lowest_max_priority_a,
-                    (in[out_idx] && !prev_out[out_idx][out_idx]) |-> out[out_idx][out_idx])
+    `BR_ASSERT_IMPL(
+        out_lowest_max_priority_a,
+        (in_internal[out_idx] && !prev_out[out_idx][out_idx]) |-> out_internal[out_idx][out_idx])
     for (
         genvar in_idx = out_idx + 1; in_idx < NumRequesters; in_idx++
     ) begin : gen_msb_input_impl_checks
       // A bit in out can only be set if the higher priority bits of the input
       // are not set or were taken by a higher priority output.
-      `BR_ASSERT_IMPL(
-          no_out_if_higher_prio_in_a,
-          out[out_idx][in_idx] |-> ((in[in_idx-1:0] & ~prev_out[out_idx][in_idx-1:0]) == '0))
+      `BR_ASSERT_IMPL(no_out_if_higher_prio_in_a,
+                      out_internal[out_idx][in_idx]
+                      |->
+                      ((in_internal[in_idx-1:0] & ~prev_out[out_idx][in_idx-1:0]) == '0))
     end
 
     if (out_idx > 0) begin : gen_secondary_out_impl_checks
       // For each result, there cannot be a bit set if there weren't bits set in
       // all previous results.
       `BR_ASSERT_IMPL(no_out_without_prev_out_a,
-                      (|out[out_idx]) |-> (num_prev_out[out_idx] == out_idx))
+                      (|out_internal[out_idx]) |-> (num_prev_out[out_idx] == out_idx))
 
       for (
           genvar in_idx = out_idx; in_idx < (NumRequesters - 1); in_idx++
@@ -155,7 +186,7 @@ module br_enc_priority_encoder #(
         // must not have taken an input of lower priority.
         `BR_ASSERT_IMPL(
             out_lower_prio_than_prev_out_a,
-            out[out_idx][in_idx] |-> (prev_out[out_idx][NumRequesters-1:in_idx+1] == '0))
+            out_internal[out_idx][in_idx] |-> (prev_out[out_idx][NumRequesters-1:in_idx+1] == '0))
       end
     end
   end
