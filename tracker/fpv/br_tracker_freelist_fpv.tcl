f49bf83f1ba756b64c1158e0f5db1f44d3c7a736
Sai Ma <masai@openai.com>
2025-02-21 00:18:47 +0000

    update fpv

--- tracker/fpv/br_tracker_freelist_fpv.tcl
+++ tracker/fpv/br_tracker_freelist_fpv.tcl
@@ -4,10 +4,8 @@ reset rst
 get_design_info
 
 # when alloc_valid is back pressured, it can not change next cycle
-cover -disable {*gen_alloc_port*br_flow_reg_rev.br_flow_checks_valid_data_impl.*unstable_c}
-cover -disable {*gen_alloc_port*br_flow_reg_rev.br_flow_checks_valid_data_intg.*unstable_c}
-cover -disable {*gen_alloc_port*br_flow_reg_fwd.br_flow_checks_valid_data_intg.*unstable_c}
-cover -disable {*gen_single_alloc_port*br_flow_reg_fwd.br_flow_checks_valid_data_intg.*unstable_c}
+cover -disable {*gen_single_alloc_port.br_flow_reg_fwd.*_unstable_c}
+cover -disable {*gen_multi_alloc_ports.br_multi_xfer_reg_fwd.*_instability_c}
 
 # prove command
 prove -all
