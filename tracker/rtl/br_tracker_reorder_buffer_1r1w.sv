a6d1e3059c87f4f5b238dbe11fe54fc3eef56ace
Ben Gelb <bgelb@openai.com>
2025-02-21 01:44:35 +0000

    format

--- tracker/rtl/br_tracker_reorder_buffer_1r1w.sv
+++ tracker/rtl/br_tracker_reorder_buffer_1r1w.sv
@@ -43,100 +43,100 @@ module br_tracker_reorder_buffer_1r1w #(
     input logic ram_rd_data_valid
 );
 
-    `BR_ASSERT_STATIC(legal_entry_id_width_a, EntryIdWidth >= MinEntryIdWidth)
+  `BR_ASSERT_STATIC(legal_entry_id_width_a, EntryIdWidth >= MinEntryIdWidth)
 
-    localparam int StagingFifoDepth = RamReadLatency + 2;
+  localparam int StagingFifoDepth = RamReadLatency + 2;
 
-    logic dealloc_complete_beat;
-    logic dealloc_complete_beat_int;
-    logic dealloc_complete_valid_int;
-    logic dealloc_complete_ready_int;
-    logic [EntryIdWidth-1:0] dealloc_complete_entry_id_int;
-    logic id_skid_fifo_pop_valid;
-    logic data_skid_fifo_pop_valid;
+  logic dealloc_complete_beat;
+  logic dealloc_complete_beat_int;
+  logic dealloc_complete_valid_int;
+  logic dealloc_complete_ready_int;
+  logic [EntryIdWidth-1:0] dealloc_complete_entry_id_int;
+  logic id_skid_fifo_pop_valid;
+  logic data_skid_fifo_pop_valid;
 
-    br_fifo_flops #(
-        .Depth(StagingFifoDepth),
-        .Width(EntryIdWidth)
-    ) id_skid_fifo (
-        .clk,
-        .rst,
-        //
-        .push_ready(dealloc_complete_ready_int),
-        .push_valid(dealloc_complete_valid_int),
-        .push_data(dealloc_complete_entry_id_int),
-        //
-        .pop_ready(dealloc_complete_beat),
-        .pop_valid(id_skid_fifo_pop_valid),
-        .pop_data(dealloc_complete_entry_id),
-        //
-        .full(),
-        .full_next(),
-        .slots(),
-        .slots_next(),
-        //
-        .empty(),
-        .empty_next(),
-        .items(),
-        .items_next()
-    );
+  br_fifo_flops #(
+      .Depth(StagingFifoDepth),
+      .Width(EntryIdWidth)
+  ) id_skid_fifo (
+      .clk,
+      .rst,
+      //
+      .push_ready(dealloc_complete_ready_int),
+      .push_valid(dealloc_complete_valid_int),
+      .push_data(dealloc_complete_entry_id_int),
+      //
+      .pop_ready(dealloc_complete_beat),
+      .pop_valid(id_skid_fifo_pop_valid),
+      .pop_data(dealloc_complete_entry_id),
+      //
+      .full(),
+      .full_next(),
+      .slots(),
+      .slots_next(),
+      //
+      .empty(),
+      .empty_next(),
+      .items(),
+      .items_next()
+  );
 
-    br_fifo_flops #(
-        .Depth(StagingFifoDepth),
-        .Width(DataWidth)
-    ) data_skid_fifo (
-        .clk,
-        .rst,
-        //
-        .push_ready(),
-        .push_valid(ram_rd_data_valid),
-        .push_data(ram_rd_data),
-        //
-        .pop_ready(dealloc_complete_beat),
-        .pop_valid(data_skid_fifo_pop_valid),
-        .pop_data(dealloc_complete_data),
-        //
-        .full(),
-        .full_next(),
-        .slots(),
-        .slots_next(),
-        //
-        .empty(),
-        .empty_next(),
-        .items(),
-        .items_next()
-    );
+  br_fifo_flops #(
+      .Depth(StagingFifoDepth),
+      .Width(DataWidth)
+  ) data_skid_fifo (
+      .clk,
+      .rst,
+      //
+      .push_ready(),
+      .push_valid(ram_rd_data_valid),
+      .push_data(ram_rd_data),
+      //
+      .pop_ready(dealloc_complete_beat),
+      .pop_valid(data_skid_fifo_pop_valid),
+      .pop_data(dealloc_complete_data),
+      //
+      .full(),
+      .full_next(),
+      .slots(),
+      .slots_next(),
+      //
+      .empty(),
+      .empty_next(),
+      .items(),
+      .items_next()
+  );
 
-    br_tracker_reorder #(
-        .NumEntries(NumEntries),
-        .EntryIdWidth(EntryIdWidth),
-        .EnableAssertFinalNotDeallocValid(EnableAssertFinalNotDeallocValid)
-    ) reorder_tracker (
-        .clk,
-        .rst,
-        //
-        .alloc_ready,
-        .alloc_valid,
-        .alloc_entry_id,
-        //
-        .dealloc_valid,
-        .dealloc_entry_id,
-        //
-        .dealloc_complete_ready(dealloc_complete_ready_int),
-        .dealloc_complete_valid(dealloc_complete_valid_int),
-        .dealloc_complete_entry_id(dealloc_complete_entry_id_int)
-    );
+  br_tracker_reorder #(
+      .NumEntries(NumEntries),
+      .EntryIdWidth(EntryIdWidth),
+      .EnableAssertFinalNotDeallocValid(EnableAssertFinalNotDeallocValid)
+  ) reorder_tracker (
+      .clk,
+      .rst,
+      //
+      .alloc_ready,
+      .alloc_valid,
+      .alloc_entry_id,
+      //
+      .dealloc_valid,
+      .dealloc_entry_id,
+      //
+      .dealloc_complete_ready(dealloc_complete_ready_int),
+      .dealloc_complete_valid(dealloc_complete_valid_int),
+      .dealloc_complete_entry_id(dealloc_complete_entry_id_int)
+  );
 
-    assign ram_wr_addr = dealloc_entry_id[MinEntryIdWidth-1:0];
-    assign ram_wr_valid = dealloc_valid;
-    assign ram_wr_data = dealloc_data;
+  assign ram_wr_addr = dealloc_entry_id[MinEntryIdWidth-1:0];
+  assign ram_wr_valid = dealloc_valid;
+  assign ram_wr_data = dealloc_data;
 
-    assign ram_rd_addr = dealloc_complete_entry_id_int[MinEntryIdWidth-1:0];
-    assign ram_rd_addr_valid = dealloc_complete_beat_int;
+  assign ram_rd_addr = dealloc_complete_entry_id_int[MinEntryIdWidth-1:0];
+  assign ram_rd_addr_valid = dealloc_complete_beat_int;
 
-    assign dealloc_complete_beat_int = dealloc_complete_valid_int && dealloc_complete_ready_int;
+  assign dealloc_complete_beat_int = dealloc_complete_valid_int && dealloc_complete_ready_int;
 
-    assign dealloc_complete_beat = dealloc_complete_valid && dealloc_complete_ready;
-    assign dealloc_complete_valid = id_skid_fifo_pop_valid && data_skid_fifo_pop_valid;
+  assign dealloc_complete_beat = dealloc_complete_valid && dealloc_complete_ready;
+  assign dealloc_complete_valid = id_skid_fifo_pop_valid && data_skid_fifo_pop_valid;
 
 endmodule
--- tracker/rtl/br_tracker_reorder_buffer_flops.sv
+++ tracker/rtl/br_tracker_reorder_buffer_flops.sv
@@ -26,66 +26,66 @@ module br_tracker_reorder_buffer_flops #(
     output logic [EntryIdWidth-1:0] dealloc_complete_entry_id,
     output logic [DataWidth-1:0] dealloc_complete_data
 );
-    localparam int MinEntryIdWidth = $clog2(NumEntries);
+  localparam int MinEntryIdWidth = $clog2(NumEntries);
 
-    logic [MinEntryIdWidth-1:0] ram_wr_addr;
-    logic ram_wr_valid;
-    logic [DataWidth-1:0] ram_wr_data;
-    logic [MinEntryIdWidth-1:0] ram_rd_addr;
-    logic ram_rd_addr_valid;
-    logic [DataWidth-1:0] ram_rd_data;
-    logic ram_rd_data_valid;
+  logic [MinEntryIdWidth-1:0] ram_wr_addr;
+  logic ram_wr_valid;
+  logic [DataWidth-1:0] ram_wr_data;
+  logic [MinEntryIdWidth-1:0] ram_rd_addr;
+  logic ram_rd_addr_valid;
+  logic [DataWidth-1:0] ram_rd_data;
+  logic ram_rd_data_valid;
 
-    br_tracker_reorder_buffer_1r1w #(
-        .NumEntries(NumEntries),
-        .EntryIdWidth(EntryIdWidth),
-        .DataWidth(DataWidth),
-        .RamReadLatency(0),
-        .EnableAssertFinalNotDeallocValid(EnableAssertFinalNotDeallocValid)
-    ) reorder_buffer (
-        .clk,
-        .rst,
-        //
-        .alloc_ready,
-        .alloc_valid,
-        .alloc_entry_id,
-        //
-        .dealloc_valid,
-        .dealloc_entry_id,
-        .dealloc_data,
-        //
-        .dealloc_complete_ready,
-        .dealloc_complete_valid,
-        .dealloc_complete_entry_id,
-        .dealloc_complete_data,
-        //
-        .ram_wr_addr,
-        .ram_wr_valid,
-        .ram_wr_data,
-        .ram_rd_addr,
-        .ram_rd_addr_valid,
-        .ram_rd_data,
-        .ram_rd_data_valid
-    );
+  br_tracker_reorder_buffer_1r1w #(
+      .NumEntries(NumEntries),
+      .EntryIdWidth(EntryIdWidth),
+      .DataWidth(DataWidth),
+      .RamReadLatency(0),
+      .EnableAssertFinalNotDeallocValid(EnableAssertFinalNotDeallocValid)
+  ) reorder_buffer (
+      .clk,
+      .rst,
+      //
+      .alloc_ready,
+      .alloc_valid,
+      .alloc_entry_id,
+      //
+      .dealloc_valid,
+      .dealloc_entry_id,
+      .dealloc_data,
+      //
+      .dealloc_complete_ready,
+      .dealloc_complete_valid,
+      .dealloc_complete_entry_id,
+      .dealloc_complete_data,
+      //
+      .ram_wr_addr,
+      .ram_wr_valid,
+      .ram_wr_data,
+      .ram_rd_addr,
+      .ram_rd_addr_valid,
+      .ram_rd_data,
+      .ram_rd_data_valid
+  );
 
-    br_ram_flops #(
-        .Depth(NumEntries),
-        .Width(DataWidth),
-        .NumReadPorts(1),
-        .NumWritePorts(1)
-    ) reorder_buffer_ram_flops (
-        .wr_clk(clk), // ri lint_check_waive SAME_CLOCK_NAME
-        .wr_rst(rst),
-        .wr_valid(ram_wr_valid),
-        .wr_addr(ram_wr_addr),
-        .wr_data(ram_wr_data),
-        .wr_word_en(1'b1),
-        .rd_clk(clk), // ri lint_check_waive SAME_CLOCK_NAME
-        .rd_rst(rst),
-        .rd_addr_valid(ram_rd_addr_valid),
-        .rd_addr(ram_rd_addr),
-        .rd_data_valid(ram_rd_data_valid),
-        .rd_data(ram_rd_data)
-    );
+  br_ram_flops #(
+      .Depth(NumEntries),
+      .Width(DataWidth),
+      .NumReadPorts(1),
+      .NumWritePorts(1)
+  ) reorder_buffer_ram_flops (
+      .wr_clk(clk),  // ri lint_check_waive SAME_CLOCK_NAME
+      .wr_rst(rst),
+      .wr_valid(ram_wr_valid),
+      .wr_addr(ram_wr_addr),
+      .wr_data(ram_wr_data),
+      .wr_word_en(1'b1),
+      .rd_clk(clk),  // ri lint_check_waive SAME_CLOCK_NAME
+      .rd_rst(rst),
+      .rd_addr_valid(ram_rd_addr_valid),
+      .rd_addr(ram_rd_addr),
+      .rd_data_valid(ram_rd_data_valid),
+      .rd_data(ram_rd_data)
+  );
 
 endmodule
