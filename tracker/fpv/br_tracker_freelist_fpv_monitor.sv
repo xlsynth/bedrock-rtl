40fcf8c7609aa259dab1532b6ae251fd3fef5cc3
Sai Ma <masai@openai.com>
2025-02-26 22:43:25 +0000

    fix FV checker after bypass change

--- tracker/fpv/br_tracker_freelist_fpv_monitor.sv
+++ tracker/fpv/br_tracker_freelist_fpv_monitor.sv
@@ -24,12 +24,22 @@ module br_tracker_freelist_fpv_monitor #(
     parameter int NumAllocPerCycle = 1,
     // Number of deallocation ports. Must be at least 1.
     parameter int NumDeallocPorts = 1,
+    // If 1, then register the alloc_sendable and alloc_entry_id outputs,
+    // improving timing at the cost of an additional cycle of cut-through latency.
+    // Note that if this is set to 0, the alloc_entry_id may be unstable
+    parameter bit RegisterAllocOutputs = 1,
+    // If 1, bypass deallocated entries to allocated entries.
+    parameter bit EnableBypass = 0,
+    // Cut-through latency of the tracker.
+    localparam int CutThroughLatency = RegisterAllocOutputs + (EnableBypass ? 0 : 1),
     // The delay between an entry being deallocated and when the deallocation
     // is indicated by dealloc_count.
     // If dealloc_count is being used to manage credit returns, this must be set
-    // so that allocation is not attemped until (2 - DeallocCountDelay) cycles
-    // after deallocation is indicated on dealloc_count. Must be >= 0.
-    parameter int DeallocCountDelay = 2,
+    // so that allocation is not attemped until (CutThroughLatency - DeallocCountDelay) cycles
+    // after deallocation is indicated on dealloc_count,
+    // where CutThroughLatency = RegisterAllocOutputs + (EnableBypass ? 0 : 1).
+    // Must be >= 0 and <= CutThroughLatency.
+    parameter int DeallocCountDelay = CutThroughLatency,
     // If 1, then assert there are no dealloc_valid bits asserted at the end of the test.
     // It is expected that alloc_valid could be 1 at end of the test because it's
     // a natural idle condition for this design.
@@ -54,6 +64,10 @@ module br_tracker_freelist_fpv_monitor #(
 );
 
   // ----------FV modeling code----------
+  // pick a random index for assertion
+  logic [AllocCountWidth-1:0] fv_idx;
+  `BR_ASSUME(fv_idx_a, $stable(fv_idx) && fv_idx < NumAllocPerCycle)
+
   logic alloc_extra_left;
   logic [AllocCountWidth-1:0] fv_allocated;
   logic [NumAllocPerCycle-1:0] fv_alloc_valid;
@@ -110,35 +124,47 @@ module br_tracker_freelist_fpv_monitor #(
     if (rst) begin
       fv_entry_used <= '0;
     end else begin
-      for (int i = 0; i < NumAllocPerCycle; i++) begin
-        if (fv_alloc_valid[i]) begin
-          fv_entry_used[alloc_entry_id[i]] <= 1'd1;
-        end
-      end
       for (int j = 0; j < NumDeallocPorts; j++) begin
         if (dealloc_valid[j]) begin
           fv_entry_used[dealloc_entry_id[j]] <= 1'd0;
         end
       end
+      for (int i = 0; i < NumAllocPerCycle; i++) begin
+        if (fv_alloc_valid[i]) begin
+          fv_entry_used[alloc_entry_id[i]] <= 1'd1;
+        end
+      end
     end
   end
 
+  // verilog_lint: waive-start line-length
   // Once an entry is allocated, the same entry cannot be allocated again until it is deallocated.
-  for (genvar i = 0; i < NumAllocPerCycle; i++) begin : gen_ast
-    `BR_ASSERT(no_entry_reuse_a, fv_alloc_valid[i] |-> !fv_entry_used[alloc_entry_id[i]])
+  if (EnableBypass) begin : gen_bypass
+    `BR_ASSERT(no_entry_reuse_a,
+               fv_alloc_valid[fv_idx] && !fv_entry_dealloc[alloc_entry_id[fv_idx]] |-> !fv_entry_used[alloc_entry_id[fv_idx]])
+    // when freelist is empty, no more alloc_sendable
+    `BR_ASSERT(freelist_empty_no_alloc_a,
+               (fv_entry_used == {NumEntries{1'b1}}) && (fv_entry_dealloc == 'd0) |-> alloc_sendable == 'd0)
+  end else begin : gen_non_bypass
+    `BR_ASSERT(no_entry_reuse_a, fv_alloc_valid[fv_idx] |-> !fv_entry_used[alloc_entry_id[fv_idx]])
+    // when freelist is empty, no more alloc_sendable
+    `BR_ASSERT(freelist_empty_no_alloc_a,
+               fv_entry_used == {NumEntries{1'b1}} |-> alloc_sendable == 'd0)
   end
-
-  // when freelist is empty, no more alloc_sendable
-  `BR_ASSERT(freelist_empty_no_alloc_a,
-             fv_entry_used == {NumEntries{1'b1}} |-> alloc_sendable == 'd0)
+  // verilog_lint: waive-stop line-length
 
   // if freelist is not empty, next cycle, some alloc_port should allocate another free entry
   `BR_ASSERT(forward_progress_a,
              (fv_entry_used | fv_entry_alloc) != {NumEntries{1'b1}} |=> alloc_sendable != 'd0)
 
   // Number of deallocations performed last cycle
-  `BR_ASSERT(dealloc_count_a, ##DeallocCountDelay dealloc_count == $past($countones(dealloc_valid),
-                                                                         DeallocCountDelay))
+  if (DeallocCountDelay == 0) begin : gen_latency0
+    `BR_ASSERT(dealloc_count_a, dealloc_count == $countones(dealloc_valid))
+  end else begin : gen_latency
+    `BR_ASSERT(
+        dealloc_count_a,
+        ##DeallocCountDelay dealloc_count == $past($countones(dealloc_valid), DeallocCountDelay))
+  end
 
   // ----------Critical Covers----------
   `BR_COVER(all_alloc_valid_c, &fv_alloc_valid)
@@ -154,6 +180,8 @@ bind br_tracker_freelist br_tracker_freelist_fpv_monitor #(
     .NumEntries(NumEntries),
     .NumAllocPerCycle(NumAllocPerCycle),
     .NumDeallocPorts(NumDeallocPorts),
+    .RegisterAllocOutputs(RegisterAllocOutputs),
+    .EnableBypass(EnableBypass),
     .DeallocCountDelay(DeallocCountDelay),
     .EnableAssertFinalNotDeallocValid(EnableAssertFinalNotDeallocValid)
 ) monitor (.*);
