e0c2d8248d8e69064642c7f3cc4d39838b53768b
Sai Ma <masai@openai.com>
2025-03-01 00:59:41 +0000

    clean FV after RTL fix

--- tracker/fpv/br_tracker_freelist_fpv.jg.tcl
+++ tracker/fpv/br_tracker_freelist_fpv.jg.tcl
@@ -18,8 +18,8 @@ reset rst
 get_design_info
 
 # when alloc_valid is back pressured, it can not change next cycle
-cover -disable {*gen_single_alloc_port.br_flow_reg_fwd.*_unstable_c}
-cover -disable {*gen_multi_alloc_ports.br_multi_xfer_reg_fwd.*_instability_c}
+cover -disable {*gen_single_alloc_port.*_unstable_c}
+cover -disable {*gen_multi_alloc_ports.*_instability_c}
 
 # prove command
 prove -all
