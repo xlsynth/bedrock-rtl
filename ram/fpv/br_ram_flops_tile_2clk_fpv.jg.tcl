eb4d4aba70925cac808dc79a2e5951641618dabc
Sai Ma <masai@openai.com>
2025-03-21 21:15:35 +0000

    add br_ram_flops_tile with different wr/rd clocks

--- ram/fpv/br_ram_flops_tile_2clk_fpv.jg.tcl
+++ ram/fpv/br_ram_flops_tile_2clk_fpv.jg.tcl
@@ -13,22 +13,26 @@
 # limitations under the License.
 
 # clock/reset set up
-clock clk
 clock wr_clk -from 1 -to 10 -both_edges
 clock rd_clk -from 1 -to 10 -both_edges
-reset rst wr_rst rd_rst
+reset wr_rst rd_rst
 
 # wr/rd primary input signals only toggle w.r.t its clock
 clock -rate {wr_rst \
             wr_valid \
             wr_addr \
             wr_data \
-            wr_word_en} wr_clk
+            wr_word_en \
+            monitor.fv_addr} wr_clk
 clock -rate {rd_rst \
             rd_addr_valid \
             rd_addr \
             rd_data_valid \
-            rd_data} rd_clk
+            rd_data \
+            monitor.fv_addr} rd_clk
+
+#TODO
+cover -disable *
 
 # prove command
 prove -all
