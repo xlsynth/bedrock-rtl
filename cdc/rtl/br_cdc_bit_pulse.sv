e2039e221bdf9d5bca155be53b2030e51d5a4b3e
Howard Mao <zhemao@openai.com>
2025-04-12 00:14:44 +0000

    Add br_cdc_bit_pulse module
    commit-id:d36037df

---
+++ cdc/rtl/br_cdc_bit_pulse.sv
@@ -0,0 +1,112 @@
+// Copyright 2025 The Bedrock-RTL Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// CDC synchronizer for a single bit pulse signal.
+
+// This module implements single-bit clock domain crossing for a pulse signal.
+// That is, a signal that is expected to infrequently be asserted high for a
+// single cycle. Single-cycle pulses on the source clock are converted to
+// single cycle pulses on the destination clock. Special handling is needed for
+// this case since switching the input of a synchronizer cell in back-to-back
+// cycles violates the stability assumptions of the synchronizer and can lead to
+// metastability.
+//
+// To accomplish the synchronization, the source pulse is converted to a level
+// signal by maintaining a source-clocked register that toggles on every pulse.
+// This level signal is then synchronized to the destination clock using a
+// standard synchronizer cell (br_cdc_bit_toggle). On the destination clock,
+// the synchronized level signal is converted back to a pulse by detecting
+// edges.
+//
+// The following timing diagram gives an example of the pulse and level signals.
+// (assumes 3-stage synchronizer)
+//
+// src_clk:   __|‾‾|__|‾‾|__|‾‾|__|‾‾|__|‾‾|__|‾‾|__|‾‾|__|‾‾|__|‾‾|_
+// src_pulse: __|‾‾‾‾‾|___________|‾‾‾‾‾|_____________________________
+// src_level: ________|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|____________________________
+// dst_clk:   ___|‾‾‾|___|‾‾‾|___|‾‾‾|___|‾‾‾|___|‾‾‾|___|‾‾‾|___|‾‾‾
+// dst_level: ___________________________|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|___________
+// dst_pulse: ___________________________|‾‾‾‾‾‾‾|_______|‾‾‾‾‾‾‾|___
+//
+// The generator of the source pulse must ensure that the pulse does not
+// occur too frequently, otherwise the stability requirement of the synchronizer
+// will be violated and metastability may occur.
+
+`include "br_asserts_internal.svh"
+`include "br_registers.svh"
+
+module br_cdc_bit_pulse #(
+    // Number of synchronization stages. Must be at least 1.
+    // WARNING: Setting this parameter correctly is critical to
+    // ensuring a low probability of metastability.
+    // The recommended value is 3 for most technology nodes.
+    // Do not decrease below that unless you have a good reason.
+    parameter int NumStages = 3,
+    parameter bit RegisterDstOutput = 0
+) (
+    input logic src_clk,
+    input logic src_rst,
+    input logic src_pulse,
+
+    // ri lint_check_waive ONE_LOCAL_CLOCK
+    input  logic dst_clk,
+    input  logic dst_rst,
+    output logic dst_pulse
+);
+  // Integration Assertions
+  // Static assertions in submodules
+
+  // TODO(zhemao): Find a way to check that pulses are far enough
+  // apart that there won't be stability issues.
+
+  // Implementation
+  logic src_level;
+  logic dst_level;
+  logic dst_level_d;
+  logic dst_pulse_internal;
+
+  // Create a level signal on the source clock that toggles
+  // on every pulse.
+  `BR_REGLX(src_level, !src_level, src_pulse, src_clk, src_rst)
+
+  br_cdc_bit_toggle #(
+      // Don't add a source flop since src_level is already
+      // a register
+      .AddSourceFlop(0),
+      .NumStages(NumStages)
+  ) br_cdc_bit_toggle_inst (
+      .src_clk,
+      .src_rst,
+      .src_bit(src_level),
+      .dst_clk,
+      .dst_rst,
+      .dst_bit(dst_level)
+  );
+
+  // Recreate the pulse on the destination clock by
+  // detecting edges
+  `BR_REGNX(dst_level_d, dst_level, dst_clk)
+
+  assign dst_pulse_internal = dst_level ^ dst_level_d;
+
+  if (RegisterDstOutput) begin : gen_register_dst_output
+    `BR_REGNX(dst_pulse, dst_pulse_internal, dst_clk)
+  end else begin : gen_no_register_dst_output
+    assign dst_pulse = dst_pulse_internal;
+  end
+
+  // Implementation assertions
+  // TODO(zhemao): Add some here
+
+endmodule
