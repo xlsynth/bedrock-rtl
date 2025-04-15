85c6be1a6d060f0c99f3bc1a99ce50ccf32442b0
Sai Ma <masai@openai.com>
2025-04-15 16:52:19 +0000

    add ctrl_push_credit

--- fifo/fpv/br_fifo_shared_dynamic_ctrl_push_credit_fpv.jg.tcl
+++ fifo/fpv/br_fifo_shared_dynamic_ctrl_push_credit_fpv.jg.tcl
@@ -14,11 +14,17 @@
 
 # clock/reset set up
 clock clk
-reset rst
+reset -none
+assume -reset -name set_rst_during_reset {rst}
+assume -bound 1 -name delay_rst {rst}
+assume -name deassert_rst {##1 !rst}
+
 get_design_info
 
 # primary input control signal should be legal during reset
-assume -name no_push_valid_during_reset {rst |-> push_valid == 'd0}
+assume -name initial_value_during_reset {rst | push_sender_in_reset |-> \
+(credit_initial_push <= Depth) && $stable(credit_initial_push)}
+assume -name no_push_valid_during_reset {rst | push_sender_in_reset |-> push_valid == 'd0}
 assume -name no_data_ram_rd_data_valid {rst | push_sender_in_reset |-> data_ram_rd_data_valid == 'd0}
 assume -name no_ptr_ram_rd_data_valid {rst | push_sender_in_reset |-> ptr_ram_rd_data_valid == 'd0}
 
