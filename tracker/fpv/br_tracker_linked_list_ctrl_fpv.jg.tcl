bb853666e8575b11c20bed90ab27523ba7d7b590
Sai Ma <masai@openai.com>
2025-04-29 10:44:56 -0700

    nightly failure is due to unreachable covers (#659)

--- tracker/fpv/br_tracker_linked_list_ctrl_fpv.jg.tcl
+++ tracker/fpv/br_tracker_linked_list_ctrl_fpv.jg.tcl
@@ -17,8 +17,7 @@ clock clk
 reset rst
 get_design_info
 
-# TODO: ignore many unreachable RTL inline covers for now
-#cover -disable {*br_tracker_reorder_buffer_flops.br_tracker_reorder_buffer_ctrl_1r1w_inst*}
+# TODO: disable covers to make nightly clean
 cover -disable *
 
 # prove command
