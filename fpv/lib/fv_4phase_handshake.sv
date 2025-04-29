80f5ec8584d7006d197382a446be290e9ae9e6ab
Sai Ma <masai@openai.com>
2025-04-29 19:53:17 +0000

    fix

--- fpv/lib/fv_4phase_handshake.sv
+++ fpv/lib/fv_4phase_handshake.sv
@@ -15,24 +15,24 @@
 module fv_4phase_handshake #(
     parameter bit Master = 1
 ) (
-    input  logic clk,
-    input  logic rst,
-    input  logic req,
-    output logic ack
+    input logic clk,
+    input logic rst,
+    input logic req,
+    input logic ack
 );
 
   if (Master) begin : gen_master
-    `BR_ASSUME(req_rise_a, $rose(req) |-> !ack)
-    `BR_ASSUME(ack_fall_a, $fell(req) |-> ack)
+    `BR_ASSUME(req_rise_a, $rose(req) |-> !ack && !$fell(ack))
+    `BR_ASSUME(req_fall_a, $fell(req) |-> ack)
     `BR_ASSUME(req_hold_until_ack_a, req && !ack |=> req)
-    `BR_ASSERT(ack_rise_a, $rose(ack) |-> req)
+    `BR_ASSERT(ack_rise_a, $rose(ack) |-> req && !$rose(req))
     `BR_ASSERT(ack_fall_a, $fell(ack) |-> !req)
     `BR_ASSERT(ack_hold_until_req_drop_a, ack && req |=> ack)
   end else begin : gen_slave
-    `BR_ASSERT(req_rise_a, $rose(req) |-> !ack)
-    `BR_ASSERT(ack_fall_a, $fell(req) |-> ack)
+    `BR_ASSERT(req_rise_a, $rose(req) |-> !ack && !$fell(ack))
+    `BR_ASSERT(req_fall_a, $fell(req) |-> ack)
     `BR_ASSERT(req_hold_until_ack_a, req && !ack |=> req)
-    `BR_ASSUME(ack_rise_a, $rose(ack) |-> req)
+    `BR_ASSUME(ack_rise_a, $rose(ack) |-> req && !$rose(req))
     `BR_ASSUME(ack_fall_a, $fell(ack) |-> !req)
     `BR_ASSUME(ack_hold_until_req_drop_a, ack && req |=> ack)
   end
