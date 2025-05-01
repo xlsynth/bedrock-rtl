671e53e82b923b31def86aa412010f2115f705ef
Trevor Bunker <tbunker@openai.com>
2025-05-01 06:02:32 +0000

    update flow

--- flow/rtl/br_flow_serializer.sv
+++ flow/rtl/br_flow_serializer.sv
@@ -200,6 +200,8 @@ module br_flow_serializer #(
     logic [SerFlitIdWidth-1:0] pop_flit_id;
 
     br_counter_incr #(
+        .ValueWidth(SerFlitIdWidth),
+        .IncrementWidth(1),
         .MaxValue(SrMinus1),
         .MaxIncrement(1),
         .EnableAssertFinalNotValid(EnableAssertFinalNotValid)
