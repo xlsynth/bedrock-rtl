ef91bbc46733df96f2e7c8533b8df472e64fe4cf
Howard Mao <zhemao@openai.com>
2025-08-05 16:57:42 +0000

    Fix covers and asserts in br_flow_serializer
    commit-id:50151d54

--- flow/fpv/serializer/br_flow_serializer_fpv.jg.tcl
+++ flow/fpv/serializer/br_flow_serializer_fpv.jg.tcl
@@ -17,12 +17,5 @@ clock clk
 reset rst
 get_design_info
 
-# RTL pop_id counter never reinit or increment by 0 (always+1)
-cover -disable {*br_counter_incr_pop_flit_id.plus_zero_a*}
-cover -disable {*br_counter_incr_pop_flit_id.reinit_and_incr_c}
-
-# TODO: disable covers to make nightly clean
-cover -disable *
-
 # prove command
 prove -all
