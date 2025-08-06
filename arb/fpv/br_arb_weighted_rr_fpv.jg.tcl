6f44d4fd3e6c7e1e7f6d7ffece4fbb8ed2bf9255
Howard Mao <zhemao@openai.com>
2025-08-06 13:01:49 -0700

    Disable unreachable counter covers in br_arb_weighted_rr in RTL instead of FPV config (#802)
    **Stack**:
    - #818
    - #817
    - #816
    - #815
    - #811
    - #810
    - #809
    - #808
    - #807
    - #806
    - #805
    - #804
    - #803
    - #802 ⬅
    - #801
    - #800
    - #799
    - #798
    
    
    ⚠️ *Part of a stack created by [spr](https://github.com/ejoffe/spr). Do
    not merge manually using the UI - doing so may have unexpected results.*

--- arb/fpv/br_arb_weighted_rr_fpv.jg.tcl
+++ arb/fpv/br_arb_weighted_rr_fpv.jg.tcl
@@ -20,13 +20,6 @@ get_design_info
 # primary output control signal should be legal during reset
 assert -name fv_rst_check_grant {rst |-> grant == 'd0}
 
-# disable unreachable RTL inline covers
-cover -disable br_arb_weighted_rr.gen_accumulated_weight\[*\].br_counter.increment_min_c
-cover -disable br_arb_weighted_rr.gen_accumulated_weight\[*\].br_counter.decrement_min_c
-cover -disable br_arb_weighted_rr.gen_accumulated_weight\[*\].br_counter.decrement_max_c
-# tied to 0 in RTL
-cover -disable br_arb_weighted_rr.gen_accumulated_weight\[*\].br_counter.reinit_and_change_c
-
 # standard use case: request will hold until grant
 task -create standard -copy_all -source_task <embedded>
 
