e9364619fdbbff603d929969b6ea4caa74c80301
Howard Mao <zhemao@openai.com>
2025-08-07 18:09:23 +0000

    Enable covers for br_ecc_secded_error_test
    commit-id:f75b2ccd

--- ecc/fpv/br_ecc_secded_error_fpv.jg.tcl
+++ ecc/fpv/br_ecc_secded_error_fpv.jg.tcl
@@ -17,11 +17,22 @@
  reset rst
  get_design_info
 
-# TODO: disable covers to make nightly clean
-cover -disable *
-
 # limit run time to 10-mins
 set_prove_time_limit 600s
+
+# se_decoder only gets correctable errors
+cover -disable *se_decoder.due_no_h_column_match_a*
+cover -disable *se_decoder.no_error_correction_a*
+cover -disable *se_decoder.due_c*
+
+# de_decoder only gets uncorrectable errors
+cover -disable *de_decoder.error_correction_a*
+cover -disable *de_decoder.ce_c*
+
+# te_decoder only gets correctable errors
+cover -disable *te_decoder.due_no_h_column_match_a*
+cover -disable *te_decoder.no_error_correction_a*
+cover -disable *te_decoder.due_c*
 
  # prove command
  prove -all
