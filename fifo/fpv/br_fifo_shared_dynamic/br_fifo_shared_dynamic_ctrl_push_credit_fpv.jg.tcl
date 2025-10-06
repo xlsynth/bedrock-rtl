6f9cfc4a237b83fa212015e7a9603dd125f14af3
Sai Ma <masai@openai.com>
2025-10-06 21:29:44 +0000

    disable more covers

--- fifo/fpv/br_fifo_shared_dynamic/br_fifo_shared_dynamic_ctrl_push_credit_fpv.jg.tcl
+++ fifo/fpv/br_fifo_shared_dynamic/br_fifo_shared_dynamic_ctrl_push_credit_fpv.jg.tcl
@@ -52,6 +52,7 @@ set NumReadPorts $param_list(NumReadPorts)
 set Depth $param_list(Depth)
 if {$Depth < 2 * $NumReadPorts} {
   cover -disable *br_fifo_shared_read_xbar*br_flow_demux_select_unstable*br_flow_checks_valid_data_impl.*stable*
+  cover -disable *br_ram_flops_tile.gen_multi_read_checks.all_rd_ports_active_a
 }
 
 # prove command
