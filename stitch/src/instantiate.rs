09ef67f1041f6c85f161e9830b53966ff0d6bca8
Mark Gottscho <mgottscho@openai.com>
2025-11-13 18:39:39 +0000

    fix

--- stitch/src/instantiate.rs
+++ stitch/src/instantiate.rs
@@ -49,22 +49,21 @@ fn create_instantiation_wrapper(
     let wrapper = ModDef::new(wrapper_name);
 
     for (i, param_set) in params.param_sets.iter().enumerate() {
-        let (inst, inst_name) = if param_set.is_empty() {
-            // Don't create a new module if there are no parameter overrides
-            let inst = wrapper.instantiate(module, None, None);
-            let inst_name = module.get_name();
-            (inst, inst_name)
-        } else {
-            let parameters: Vec<(&str, BigInt)> = param_set.iter().map(
-                |(k, v)| (k.as_str(), BigInt::from_str(v.as_str()).unwrap())
-            ).collect();
-            let parameterized_mod = module.parameterize(&parameters);
-            let parameterized_mod_name = format!("{}_{}", module.get_name(), i);
-            let inst = wrapper.instantiate(&parameterized_mod, Some(&parameterized_mod_name), None);
-            let inst_name = parameterized_mod_name;
+        let (inst, inst_name) = {
+            let module = if param_set.is_empty() {
+                module
+            } else {
+                let parameters: Vec<(&str, BigInt)> = param_set.iter().map(
+                    |(k, v)| (k.as_str(), BigInt::from_str(v.as_str()).unwrap())
+                ).collect();
+                &module.parameterize(&parameters)
+            };
+            let inst_name = format!("{}_{}", module.get_name(), i);
+            let inst = wrapper.instantiate(module, Some(&inst_name), None);
             (inst, inst_name)
         };
 
+        // TODO: consider using wrap() instead
         for port in inst.get_ports(None) {
             let port_name = port.name();
             port.export_as(format!("{inst_name}__{port_name}"));
