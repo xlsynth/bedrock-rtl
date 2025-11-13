dadaeba2f5e5bfdd379d3acb49afbc827911ea64
Sai Ma <masai@openai.com>
2025-11-12 17:16:04 -0800

    Add NumRequest = 1 case (#932)
    1. Updated FV `BR_FV_2RAND_IDX(i, j, NumRequesters) to get two random
    indices since we can have a single request now.
    2. when NumRequesters = 1, some assumptions/cover become unreachable,
    because that request will always be granted.

--- arb/rtl/br_arb_lru.sv
+++ arb/rtl/br_arb_lru.sv
@@ -26,8 +26,9 @@ module br_arb_lru #(
   //------------------------------------------
   // Integration checks
   //------------------------------------------
-
-  `BR_COVER_INTG(request_multihot_c, !$onehot0(request))
+  if (NumRequesters > 1) begin : gen_multi_req
+    `BR_COVER_INTG(request_multihot_c, !$onehot0(request))
+  end
 
   //------------------------------------------
   // Implementation
