c64c42a7bf1eabaf7c893977f0e5a27c040854db
Howard Mao <zhemao@openai.com>
2025-12-08 09:32:41 -0800

    Default flow_demux/xbar select/destination stability to match data stability (#879)
    Usually, the select/destination is part of the data packet, so it makes
    sense to have the defaults like this to reduce the number of parameter
    overrides needed to close coverage.
    
    ---
    
    **Stack**:
    - #879 ⬅
    - #878
    
    
    ⚠️ *Part of a stack created by [spr](https://github.com/ejoffe/spr). Do
    not merge manually using the UI - doing so may have unexpected results.*

--- flow/rtl/br_flow_demux_select_unstable.sv
+++ flow/rtl/br_flow_demux_select_unstable.sv
@@ -35,7 +35,7 @@ module br_flow_demux_select_unstable #(
     parameter bit EnableAssertPushDataStability = EnableAssertPushValidStability,
     // If 1, assert that select is stable when push is backpressured.
     // If 0, cover that select can be unstable.
-    parameter bit EnableAssertSelectStability = 0,
+    parameter bit EnableAssertSelectStability = EnableAssertPushDataStability,
     // If 1, assert that push_data is always known (not X) when push_valid is asserted.
     parameter bit EnableAssertPushDataKnown = 1,
     // If 1, then assert there are no valid bits asserted at the end of the test.
