31e2fcc7afec9b6cd3b6b5789511e08d69db84f0
Sai Ma <masai@openai.com>
2026-03-24 21:03:02 +0000

    add axi protocol checker

---
+++ amba/fpv/br_amba_axi_shrinker_fpv.jg.tcl
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: Apache-2.0
+
+
+# clock/reset set up
+clock clk
+reset rst
+get_design_info
+
+# limit run time to 30-mins
+set_prove_time_limit 1800s
+
+# prove command
+prove -all
