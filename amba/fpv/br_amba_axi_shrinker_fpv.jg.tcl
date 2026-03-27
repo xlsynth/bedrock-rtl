4d19ace077615ca80173479bf85c73b368ecc005
Sai Ma <masai@openai.com>
2026-03-27 18:26:44 +0000

    param sweep

--- amba/fpv/br_amba_axi_shrinker_fpv.jg.tcl
+++ amba/fpv/br_amba_axi_shrinker_fpv.jg.tcl
@@ -7,7 +7,7 @@ reset rst
 get_design_info
 
 # limit run time to 30-mins
-set_prove_time_limit 2h
+set_prove_time_limit 30m
 
 # fv_fifo is a bit oversized
 cover -disable *monitor.w_fifo.gen_Bypass_ast.no_push_full_a:precondition1
