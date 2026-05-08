351ff2c5e36a2fd22fa7376e1e52c36e06e9c908
Mark Gottscho <mgottscho@openai.com>
2026-05-08 11:03:07 -0700

    fix

--- mux/rtl/br_mux_onehot.sv
+++ mux/rtl/br_mux_onehot.sv
@@ -22,7 +22,8 @@ module br_mux_onehot #(
     parameter int SymbolWidth = 1,
     // If 1, assert that the select is, in fact, onehot.
     // If 0, the select can be multi-hot, but in this case,
-    // the output will be undefined and driven to X in simulation.
+    // the output will be undefined and driven to X in simulation,
+    // unless NumSymbolsIn == 1.
     parameter bit EnableAssertSelectOnehot = 1
 ) (
     // Ignored when NumSymbolsIn == 1.
@@ -46,25 +47,10 @@ module br_mux_onehot #(
   //------------------------------------------
 
   if (NumSymbolsIn == 1) begin : gen_base
-`ifdef SIMULATION
-    if (EnableAssertSelectOnehot) begin : gen_onehot_out
-      assign out = in;
-    end else begin : gen_multihot_out
-      always_comb begin
-        if (select == 1'b0) begin
-          out = in;
-        end else begin
-          out = 'X;
-        end
-      end
-    end
-`else  // SIMULATION
     `BR_UNUSED(select)
     assign out = in;
-`endif  // SIMULATION
 
   end else begin : gen_n
-
     logic [SymbolWidth-1:0] out_internal;
 
     always_comb begin
