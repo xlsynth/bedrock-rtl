54a9bdb98f18e5349e5e006a4c767a5e44e8bf1c
Sai Ma <masai@openai.com>
2026-07-06 12:17:17 -0700

    add comment

--- macros/br_asserts_in_pkg_test.sv
+++ macros/br_asserts_in_pkg_test.sv
@@ -12,6 +12,8 @@ package br_asserts_test_pkg;
   localparam int Width = 4;
   `BR_ASSERT_STATIC_IN_PACKAGE(width_check_a, Width == 4)
 `ifdef BR_ENABLE_FPV
+  // This intentionally false assertion must elaborate under FPV, verifying that
+  // BR_ASSERT_STATIC_IN_PACKAGE expands to a no-op when BR_ENABLE_FPV is set.
   `BR_ASSERT_STATIC_IN_PACKAGE(fp_static_asserts_disabled_a, 0)
 `endif
 endpackage : br_asserts_test_pkg
