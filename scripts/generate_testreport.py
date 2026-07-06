d6861ca1239d182bd2a5bfcceb7998e880a0e480
Karol Gugala <kgugala@antmicro.com>
2026-07-06 19:50:06 +0200

    Introduce testplanner as a way of deploying report (#1212)
    Add testsuite report generation scripts. They generate .html site, using
    tesplanner, on which the test results are represented.
    
    Signed-off-by: Kornel Uriasz <kuriasz@antmicro.com>
    Co-authored-by: Kornel Uriasz <kuriasz@antmicro.com>

---
+++ scripts/generate_testreport.py
@@ -0,0 +1,123 @@
+#!/bin/python
+
+from dataclasses import dataclass
+
+import re
+import os
+import json
+import sys
+
+
+@dataclass
+class TestResult:
+    category: str
+    postfix: str
+    name: str
+    result: str
+    time: str
+
+
+def read_timestamp(timestamp_file):
+    return open(timestamp_file, "r").read().rstrip()
+
+
+def get_testplans(test_results):
+    tp = set()
+
+    for result in test_results:
+        tp.add(result.category)
+
+    return tp
+
+
+def create_testresult_path(test_result):
+    path = "testlogs/" + test_result.category + "/"
+
+    if test_result.postfix:
+        path += test_result.postfix + "/"
+
+    path += test_result.name + "/"
+    path += "test.log"
+
+    return path
+
+
+def get_simulated_time(test_result):
+    testlog = f"../{create_testresult_path(test_result)}"
+
+    pattern = re.compile(
+        r""".*?\$finish\s+at\s+(?P<sim_time>[\d.]+[a-zuns]+)\s*;""",
+        re.VERBOSE | re.IGNORECASE,
+    )
+
+    with open(testlog) as f:
+        for line in f:
+            match = pattern.match(line)
+            if match:
+                return match.group("sim_time")
+
+    return "N.A."
+
+
+def create_test_entry(test_result):
+    passed = 1 if test_result.result == "PASSED" else 0
+    simulated_time = get_simulated_time(test_result)
+    return {
+        "name": test_result.name,
+        "passing": passed,
+        "total": 1,
+        "job_runtime": float(test_result.time),
+        "simulated_time": simulated_time,
+    }
+
+
+def generate_testreport(input_file, timestamp):
+    if not os.path.exists("./testreports"):
+        os.makedirs("./testreports")
+
+    pattern = re.compile(
+        r"""\s*//(?P<category>[^/]+?)(?:/(?P<postfix>[^:]+?))?:(?P<name>.*?)
+                             \s+(?P<result>.*?)\s+in\s+(?P<time>.*?)s""",
+        re.VERBOSE,
+    )
+    test_results = list()
+
+    with open(input_file) as f:
+        for line in f:
+            match = pattern.match(line)
+            if match == None:
+                print("Regex couldn't match the expression on line: " + line, end="")
+                sys.exit(-1)
+
+            result = TestResult(
+                category=match.group("category"),
+                postfix=match.group("postfix"),
+                name=match.group("name"),
+                result=match.group("result"),
+                time=match.group("time"),
+            )
+            test_results.append(result)
+
+    tps = get_testplans(test_results)
+
+    for tp in tps:
+        hjson_out = {"timestamp": timestamp, "test_results": []}
+        output_file = open(f"./testreports/testreport_{tp}.hjson", "w")
+
+        for result in test_results:
+            if tp == result.category:
+                hjson_out["test_results"].append(create_test_entry(result))
+
+        json.dump(hjson_out, output_file, indent=4)
+        output_file.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage " + sys.argv[0] + " <bazel_test_results> <tests_timestamp>")
+        sys.exit(-1)
+
+    input_file = sys.argv[1]
+    timestamp = read_timestamp(sys.argv[2])
+
+    generate_testreport(input_file, timestamp)
